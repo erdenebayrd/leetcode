72ae0f7e8fca20bb5b1943b745a604165ba5041e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-03-22 23:09:56 +1100

    Time: 52 ms (27.66%), Space: 21 MB (8.51%) - LeetHub

---
+++ 2880-find-the-closest-marked-node/2880-find-the-closest-marked-node.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumDistance(self, n: int, edges: List[List[int]], s: int, marked: List[int]) -> int:
+        # dijkstra
+        inf = int(1e9)
+        graph = defaultdict(list)
+        distArr = [[inf] * n for _ in range(n)]
+        for u, v, w in edges:
+            distArr[u][v] = min(distArr[u][v], w)
+        for u, v, w in edges:
+            graph[u].append((v, distArr[u][v]))
+        
+        def dijkstra():
+            dist = [inf] * n
+            dist[s] = 0
+            arr = [(dist[s], s)]
+            heapq.heapify(arr)
+            while len(arr) > 0:
+                _, u = heapq.heappop(arr)
+                for v, w in graph[u]:
+                    if dist[v] > dist[u] + w:
+                        dist[v] = dist[u] + w
+                        heapq.heappush(arr, (dist[v], v))
+            return dist
+        
+        dist = dijkstra()
+        res = inf
+        for v in marked:
+            res = min(res, dist[v])
+        if res == inf:
+            res = -1
+        
+        return res
