ada09e27f3cd32a59f96a2e5edc03617212eb7d2
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-03-25 14:20:24 +1100

    Time: 22 ms (21.03%), Space: 23.1 MB (42.86%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -5,30 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.res = int(-1e9)
+
+    def dfs(self, cur) -> int:
+        if cur is None:
+            return 0
+        le = max(self.dfs(cur.left), 0)
+        ri = max(self.dfs(cur.right), 0)
+        self.res = max(self.res, le + ri + cur.val)
+        return max(le + cur.val, ri + cur.val)
+
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        maxPath = -float("inf")
-
-        # post order traversal of subtree rooted at `node`
-        def gainFromSubtree(node: Optional[TreeNode]) -> int:
-            nonlocal maxPath
-
-            if not node:
-                return 0
-
-            # add the gain from the left subtree. Note that if the
-            # gain is negative, we can ignore it, or count it as 0.
-            # This is the reason we use `max` here.
-            gainFromLeft = max(gainFromSubtree(node.left), 0)
-
-            # add the gain / path sum from right subtree. 0 if negative
-            gainFromRight = max(gainFromSubtree(node.right), 0)
-
-            # if left or right gain are negative, they are counted
-            # as 0, so this statement takes care of all four scenarios
-            maxPath = max(maxPath, gainFromLeft + gainFromRight + node.val)
-
-            # return the max sum for a path starting at the root of subtree
-            return max(gainFromLeft + node.val, gainFromRight + node.val)
-
-        gainFromSubtree(root)
-        return maxPath
+        self.dfs(root)
+        return self.res
