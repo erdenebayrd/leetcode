a9a18c277ef1b6d733157334746754ce798768ee
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-03-26 12:44:12 +1100

    Time: 55 ms (62.12%), Space: 19.8 MB (47.67%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,27 +1,16 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # time: O(N)
+        # space: O(1)
+        # method: sliding window
         n = len(nums)
-        def mostRight(curIdx: int, curVal: int) -> tuple:
-            if curVal >= k:
-                return (-1, -1)
-            for i in range(curIdx + 1, n):
-                curVal *= nums[i]
-                if curVal >= k:
-                    return (i - 1, curVal // nums[i])
-            return (n - 1, curVal)
-        
-        idx, val = mostRight(0, nums[0])
-        res = idx + 1
-        for i in range(1, n):
-            if i > idx:
-                val = nums[i]
-                idx = i
-            else:
-                val = val // nums[i - 1]
-            idx, val = mostRight(idx, val)
-            print(idx)
-            if idx == -1:
-                continue
-            res += idx - i + 1
+        curProduct = 1
+        l = 0
+        res = 0
+        for r in range(n):
+            curProduct *= nums[r]
+            while l <= r and curProduct >= k:
+                curProduct //= nums[l]
+                l += 1
+            res += (r - l + 1)
         return res
-        
