314ab524266330d33435bdd7349f2d2ca5c0ac1e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-03-30 10:50:52 +1100

    Time: 83 ms (19.06%), Space: 18.3 MB (21.87%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -3,8 +3,8 @@ class Solution:
         res, cnt = 1, 0
         for ch in s:
             x = ord(ch) - ord('a')
-            if (cnt >> x) & 1:
+            if cnt >> x & 1:
                 res += 1
                 cnt = 1 << x
-            cnt |= (1 << x)
+            cnt |= 1 << x
         return res
