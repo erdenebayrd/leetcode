c85cc2daba69672a2273cb3937d66a6fd3b71946
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-08 11:13:09 +1000

    Time: 2 ms (74.36%), Space: 17.9 MB (48.49%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -10,8 +10,4 @@ class Solution:
         if idx == -1:
             return 0
         cnt = 0
-        for i in range(2, len(nums), 3):
-            cnt += 1
-            if i >= idx:
-                return cnt
-        return (len(nums) + 2) // 3
+        return idx // 3 + 1
