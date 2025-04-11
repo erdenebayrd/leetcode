21187cbb8706b58a50f61c99b153e4fa1c361aac
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-11 10:47:41 +1000

    Time: 496 ms (82.48%), Space: 18 MB (13.03%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -1,7 +1,7 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        def check(x: int) -> bool:
-            n = int(log(x, 10)) + 1
+        # brute force
+        def check(x: int, n: int) -> bool:
             s = 0
             for i in range(n // 2):
                 s += (x % 10)
@@ -13,8 +13,9 @@ class Solution:
 
         res = 0
         for x in range(low, high + 1):
-            if (int(log(x, 10)) + 1) & 1:
+            n = int(log(x, 10)) + 1
+            if n & 1:
                 continue
-            if check(x) is True:
+            if check(x, n) is True:
                 res += 1
         return res
