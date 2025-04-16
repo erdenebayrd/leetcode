ead510a4730c7c7465e6e4439a3047823634b33b
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-16 10:44:04 +1000

    Time: 329 ms (5.18%), Space: 34.3 MB (94.5%) - LeetHub

---
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        cnt = defaultdict(int)
+
+        def nSum(n: int) -> int:
+            return n * (n - 1) // 2
+
+        def diff(prevCnt: int, curCnt: int) -> int:
+            assert abs(prevCnt - curCnt) == 1
+            return nSum(curCnt) - nSum(prevCnt)
+        
+        def calc(x: int, val: int) -> int:
+            prevCnt = cnt[x]
+            cnt[x] += val
+            curCnt = cnt[x]
+            return diff(prevCnt, curCnt)
+
+        res, j, curK, n = 0, 0, 0, len(nums)
+        for i in range(n):
+            while j < n and curK < k:
+                curK += calc(nums[j], 1)
+                j += 1
+            if curK >= k:
+                res += n - j + 1
+            curK += calc(nums[i], -1)
+        return res
