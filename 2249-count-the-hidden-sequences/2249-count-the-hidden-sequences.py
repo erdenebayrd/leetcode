b87712e3204c18810689188de7e5898c0a3dc511
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-21 12:54:25 +1000

    Time: 170 ms (17.17%), Space: 32.3 MB (57.58%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
@@ -1,11 +1,17 @@
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        # time: O(N)
+        # space: O(1)
         n = len(differences)
         m = n + 1
         res = 0
-        hidden = [0] * m
+        hidden = 0
+        mn = 0
+        mx = 0
         for i in range(1, m):
-            hidden[i] = differences[i - 1] + hidden[i - 1]
-        delta = lower - min(hidden)
-        res = max(res, upper - max(hidden) - delta + 1)
+            hidden = differences[i - 1] + hidden
+            mn = min(mn, hidden)
+            mx = max(mx, hidden)
+        delta = lower - mn
+        res = max(res, upper - mx - delta + 1)
         return res
