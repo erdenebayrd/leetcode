fe5559c9afb7834f3caedc53a9ac23c28038d723
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-22 14:11:26 +1000

    Time: 1547 ms (18.77%), Space: 43 MB (44.65%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
@@ -1,35 +1,42 @@
 class FenwickTree:
-    def __init__(self, size):
-        self.tree = [0] * (size + 1)
-
-    def update(self, index, delta):
-        index += 1
-        while index <= len(self.tree) - 1:
-            self.tree[index] += delta
-            index += index & -index
-
-    def query(self, index):
-        index += 1
+    def __init__(self, sz: int):
+        self.ft = [0] * (sz + 1)
+    
+    def update(self, pos: int, delta: int) -> None:
+        assert pos > 0 and pos < len(self.ft)
+        while pos < len(self.ft):
+            self.ft[pos] += delta
+            pos += -pos & pos
+    
+    def query(self, pos: int) -> int:
+        assert pos > 0 and pos < len(self.ft)
         res = 0
-        while index > 0:
-            res += self.tree[index]
-            index -= index & -index
+        while pos > 0:
+            res += self.ft[pos]
+            pos -= -pos & pos
         return res
 
 class Solution:
-    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums1)
-        pos2, reversedIndexMapping = [0] * n, [0] * n
-        for i, num2 in enumerate(nums2):
-            pos2[num2] = i
-        for i, num1 in enumerate(nums1):
-            reversedIndexMapping[pos2[num1]] = i
-        tree = FenwickTree(n)
+    def goodTriplets(self, a: List[int], b: List[int]) -> int:
+        # very similar to https://www.spoj.com/CSMS/problems/ERD0002/
+        # time: O(N * Log N)
+        # space: O(N)
+        # method: Fenwick tree + something
+        n = len(a)
+        bPos = {}
+        for i in range(n):
+            bPos[b[i]] = i + 1
+        prefixFt = FenwickTree(n)
+        suffixFt = FenwickTree(n)
+        for i in range(n):
+            suffixFt.update(i + 1, 1)
         res = 0
-        for value in range(n):
-            pos = reversedIndexMapping[value]
-            left = tree.query(pos)
-            tree.update(pos, 1)
-            right = (n - 1 - pos) - (value - left)
-            res += left * right
+        for i in range(n):
+            idx = bPos[a[i]]
+            suffixFt.update(idx, -1)
+            # calc
+            leftCount = prefixFt.query(idx)
+            rightCount = suffixFt.query(n) - suffixFt.query(idx)
+            res += leftCount * rightCount
+            prefixFt.update(idx, 1)
         return res
