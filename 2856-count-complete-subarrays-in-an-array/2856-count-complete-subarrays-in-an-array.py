6c1f9bd4a42d73711bfd3da44f6a44fb837b9541
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-24 10:50:38 +1000

    Time: 20 ms (40.04%), Space: 18.3 MB (5.77%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,21 +1,28 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        # time: O(N^2)
+        # time: O(N)
         # space: O(N)
-        # method: brute force
+        # method: two pointers
         n = len(nums)
         cnt = defaultdict(int)
         for num in nums:
             cnt[num] += 1
         m = len(cnt)
         res = 0
-        for i in range(n):
-            cnt = defaultdict(int)
-            cur = 0
-            for j in range(i, n):
+        cnt = defaultdict(int)
+        cur = 0
+        i, j = 0, 0
+        while j < n:
+            while j < n and cur < m:
                 cnt[nums[j]] += 1
                 if cnt[nums[j]] == 1:
                     cur += 1
-                if cur >= m:
-                    res += 1
+                j += 1
+            # print(i, j)
+            while i < n and cur >= m:
+                res += n - j + 1
+                cnt[nums[i]] -= 1
+                if cnt[nums[i]] == 0:
+                    cur -= 1
+                i += 1
         return res
