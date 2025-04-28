88a1584ab25e112c8a13b3d844577994b590b110
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-28 12:30:00 +1000

    Time: 196 ms (9.19%), Space: 30.6 MB (42.08%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -1,23 +1,26 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        # time: O(N * LogN)
+        # time: O(N)
         # space: O(1)
+        # method: two pointers
         n = len(nums)
         for i in range(1, n):
             nums[i] += nums[i - 1]
-        
+        right = 0
+        cur = 0
         res = 0
-        for i in range(n):
-            lo, hi = i - 1, n
-            while lo + 1 < hi:
-                md = (lo + hi) // 2
-                x = nums[md]
-                if i > 0:
-                    x -= nums[i - 1]
-                if x * (md - i + 1) < k:
-                    lo = md
+        for left in range(n):
+            if left > right:
+                right = left
+            while right < n:
+                sz = right - left + 1
+                cur = nums[right]
+                if left > 0:
+                    cur -= nums[left - 1]
+                cur *= sz
+                if cur < k:
+                    right += 1
                 else:
-                    hi = md
-            # print(i, ":", lo)
-            res += lo - i + 1
+                    break
+            res += right - left
         return res
