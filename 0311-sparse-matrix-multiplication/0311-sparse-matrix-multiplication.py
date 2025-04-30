0c2e03b14ed58d9b83a600c44a59ab20cef199a6
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-04-30 10:20:10 +1000

    Time: 185 ms (6.7%), Space: 29.5 MB (6.42%) - LeetHub

---
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        import numpy as np
+        
+        m1 = np.array(mat1)
+        m2 = np.array(mat2)
+        res = np.dot(m1, m2)
+        return res.tolist()
