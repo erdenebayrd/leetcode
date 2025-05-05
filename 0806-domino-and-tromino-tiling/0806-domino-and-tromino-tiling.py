c64690bf9ee99859a93d3ab067fe07d47bab62c3
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-05-05 10:44:11 +1000

    Time: 16 ms (8.98%), Space: 20.3 MB (5.05%) - LeetHub

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        mod = int(1e9) + 7
+        
+        @cache
+        def solve(idx: int, bit: int) -> int:
+            if idx >= n:
+                if idx == n and bit == 0:
+                    return 1
+                return 0
+            if bit == 0:
+                res = solve(idx + 1, 0) % mod
+                res = (res + solve(idx + 2, 0)) % mod
+                res = (res + solve(idx + 1, 1)) % mod
+                res = (res + solve(idx + 1, 2)) % mod
+            elif bit == 1:
+                res = solve(idx + 2, 0) % mod
+                res = (res + solve(idx + 1, 2)) % mod
+            elif bit == 2:
+                res = solve(idx + 2, 0) % mod
+                res = (res + solve(idx + 1, 1)) % mod
+            return res
+
+        return solve(0, 0)
