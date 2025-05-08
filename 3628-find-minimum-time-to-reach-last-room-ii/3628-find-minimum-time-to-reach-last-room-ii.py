b2034114871b98054bcf7ab0307e21550305bb87
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-05-08 10:45:12 +1000

    Time: 4379 ms (8.58%), Space: 125.4 MB (42.92%) - LeetHub

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        # time: O(N * LogN)
+        # space: O(N)
+        # method: dijkstra with double graph (cost)
+        inf = int(2e9)
+        n = len(moveTime)
+        m = len(moveTime[0])
+        cost = [[[inf] * 2 for _ in range(m)] for _ in range(n)]
+        cost[0][0][1] = 0
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, -1, 1]
+        moveTime[0][0] = 0
+
+        def coordinateToNode(x: int, y: int) -> int:
+            return x * m + y
+
+        def nodeToCoordinate(x: int) -> List[int]:
+            return [x // m, x % m]
+        
+        pq = [(cost[0][0][1], 0, 1)]
+        heapq.heapify(pq)
+        while len(pq) > 0:
+            _, node, preState = heapq.heappop(pq)
+            x, y = nodeToCoordinate(node)
+            curState = preState ^ 1
+            for i in range(4):
+                curx = dx[i] + x
+                cury = dy[i] + y
+                if curx < 0 or curx >= n or cury < 0 or cury >= m:
+                    continue
+                curCost = max(cost[x][y][preState], moveTime[curx][cury]) + curState + 1
+                if cost[curx][cury][curState] > curCost:
+                    cost[curx][cury][curState] = curCost
+                    heapq.heappush(pq, (cost[curx][cury][curState], coordinateToNode(curx, cury), curState))
+        # print(cost)
+        return min(cost[n - 1][m - 1])
