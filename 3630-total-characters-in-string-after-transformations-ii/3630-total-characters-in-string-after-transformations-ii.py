b69840bd691f46b4ebaaa76cfdeef4a9528dbf85
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-05-14 12:09:04 +1000

    Time: 1374 ms (65.91%), Space: 29.9 MB (38.64%) - LeetHub

--- 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.py
+++ 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.py
@@ -4,6 +4,16 @@ class Solution:
     def __init__(self):
         self.__MOD = int(1e9 + 7)
 
+    def matrix_power_mod(self, A, power):
+        result = np.identity(A.shape[0], dtype=object)
+        A = A % self.__MOD
+        while power > 0:
+            if power & 1:
+                result = np.matmul(result, A) % self.__MOD
+            A = np.matmul(A, A) % self.__MOD
+            power >>= 1
+        return result
+
     def lengthAfterTransformations(self, s: str, t: int, nums: List[int]) -> int:
         cnt = Counter(s)
         F = np.array([[cnt[chr(idx + ord('a'))]] for idx in range(26)])
@@ -15,18 +25,8 @@ class Solution:
                 T[(i + j + 1) % 26, i] = 1
         # print(T)
         
-        def matrix_power_mod(A, power):
-            result = np.identity(A.shape[0], dtype=object)
-            A = A % self.__MOD
-            while power > 0:
-                if power & 1:
-                    result = np.matmul(result, A) % self.__MOD
-                A = np.matmul(A, A) % self.__MOD
-                power >>= 1
-            return result
-        
         # T = np.linalg.matrix_power(T, t)
-        T = matrix_power_mod(T, t)
+        T = self.matrix_power_mod(T, t)
         result = T @ F % self.__MOD
         # print(result)
         return result.sum() % self.__MOD
