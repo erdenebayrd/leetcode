966903ad90f26811b27ce005e94e85dba0a1b410
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-05-18 23:28:17 +1000

    Time: 3470 ms (7.78%), Space: 87.9 MB (14.44%) - LeetHub

---
+++ 2061-painting-a-grid-with-three-different-colors/2061-painting-a-grid-with-three-different-colors.py
@@ -0,0 +1,37 @@
+# from itertools import product
+
+class Solution:
+
+    @cache
+    def solve(self, comb, idx):
+        if idx >= self.n:
+            return 1
+        res = 0
+        for row in self.combinations:
+            flag = True
+            for i in range(len(row)):
+                if row[i] == comb[i]:
+                    flag = False
+            if flag is False:
+                continue
+            # res = (res + self.solve(row, idx + 1)) % self.mod
+            res = (res + self.solve(row, idx + 1)) % self.mod
+        return res
+
+
+    def colorTheGrid(self, m: int, n: int) -> int:
+        self.mod = int(1e9 + 7)
+        self.n = n
+        self.combinations = []
+        for row in list(product([0, 1, 2], repeat=m)):
+            flag = True
+            for i in range(1, len(row)):
+                if row[i] == row[i - 1]:
+                    flag = False
+            if flag is False:
+                continue
+            self.combinations.append(row)
+        res = 0
+        for row in self.combinations:
+            res = (res + self.solve(row, 1)) % self.mod
+        return res
