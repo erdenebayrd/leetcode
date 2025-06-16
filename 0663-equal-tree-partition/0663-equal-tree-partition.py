128cc280df75c80d0540367d2f356011371fe8ac
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-06-16 14:39:14 +1000

    Time: 7 ms (45.24%), Space: 25.6 MB (7.14%) - LeetHub

---
+++ 0663-equal-tree-partition/0663-equal-tree-partition.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkEqualTree(self, root: Optional[TreeNode]) -> bool:
+        @cache
+        def dfs(cur) -> int:
+            res = cur.val
+            if cur.left is not None:
+                res += dfs(cur.left)
+            if cur.right is not None:
+                res += dfs(cur.right)
+            return res
+        total = dfs(root)
+        if total & 1:
+            return False
+        half = total // 2
+        
+        def check(cur, isRoot: False) -> bool:
+            if dfs(cur) == half and isRoot is False:
+                return True
+            res = False
+            if cur.left is not None:
+                res |= check(cur.left, False)
+            if cur.right is not None:
+                res |= check(cur.right, False)
+            return res
+        return check(root, True)
