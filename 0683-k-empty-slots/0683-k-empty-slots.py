a241fa15ce0ff080eebc483052abfeff947e87e3
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-06-22 21:13:16 +1000

    Time: 300 ms (21.85%), Space: 19.7 MB (66.89%) - LeetHub

--- 0683-k-empty-slots/0683-k-empty-slots.py
+++ 0683-k-empty-slots/0683-k-empty-slots.py
@@ -17,9 +17,6 @@ class FenwickTree:
     def rangeSum(self, l: int, r: int) -> int:
         assert l - 1 >= 0
         return self.prefixSum(r) - self.prefixSum(l - 1)
-    
-    def get(self, pos: int) -> int:
-        return self.rangeSum(pos, pos)
 
 class Solution:
     def kEmptySlots(self, bulbs: List[int], k: int) -> int:
@@ -27,8 +24,8 @@ class Solution:
         ft = FenwickTree(defaultValue = 0, sz = n)
         for day, pos in enumerate(bulbs):
             ft.update(pos, 1)
-            if pos - k - 1 > 0 and ft.get(pos - k - 1) == 1 and ft.rangeSum(pos - k, pos - 1) == 0:
+            if pos - k - 1 > 0 and ft.rangeSum(pos - k - 1, pos - k - 1) == 1 and ft.rangeSum(pos - k, pos - 1) == 0:
                 return day + 1
-            if pos + k + 1 <= n and ft.get(pos + k + 1) == 1 and ft.rangeSum(pos + 1, pos + k) == 0:
+            if pos + k + 1 <= n and ft.rangeSum(pos + k + 1, pos + k + 1) == 1 and ft.rangeSum(pos + 1, pos + k) == 0:
                 return day + 1
         return -1
