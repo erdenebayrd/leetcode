cb9e6c60ce24bf96880e33c8c5456f063029b007
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-06-26 10:44:28 +1000

    Time: 3 ms (69.92%), Space: 17.8 MB (66.67%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -1,14 +1,11 @@
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
         n = len(s)
-        res = 0
+        res = Counter(s)["0"]        
         for i in range(min(32, n)):
             if s[n - 1 - i] == "1":
                 k -= (1 << i)
                 if k < 0:
                     break
-                res += 1
-        for ch in s:
-            if ch == "0":
                 res += 1
         return res
