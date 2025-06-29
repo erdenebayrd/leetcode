b944553afa887e132f6f11e5babfa093df2517bb
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-06-29 10:56:35 +1000

    Time: 1230 ms (45.75%), Space: 27.9 MB (61.72%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -0,0 +1,45 @@
+class Solution:
+    def __init__(self) -> None:
+        self.__mod = 10 ** 9 + 7
+    
+    def powerOfTwo(self, n: int) -> int:
+        assert n >= 0
+        res = 1
+        base = 2
+        while n > 0:
+            if n & 1:
+                res = (res * base) % self.__mod
+            n //= 2
+            base = (base * base) % self.__mod
+        return res
+
+
+    def prefixSum(self, n: int) -> int:
+        # return sum of power of 2 -> 2 ^ 0 + 2 ^ 1 + ... + 2 ^ n
+        return (self.powerOfTwo(n + 1) - 1 + self.__mod) % self.__mod
+    
+    def rangeSum(self, l: int, r: int) -> int:
+        if l == 0:
+            return self.prefixSum(r)
+        return (self.prefixSum(r) - self.prefixSum(l - 1) + self.__mod) % self.__mod
+
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            lo, hi = -1, i + 1
+            while lo + 1 < hi:
+                md = (lo + hi) // 2
+                if nums[md] + nums[i] <= target:
+                    lo = md
+                else:
+                    hi = md
+            if lo == i:
+                res = (res + 1) % self.__mod
+                lo -= 1
+            if lo == -1:
+                continue
+            res = (res + self.rangeSum(i - lo - 1, i - 1)) % self.__mod
+            
+        return res
