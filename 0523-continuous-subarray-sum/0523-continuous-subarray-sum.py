5587c95faa966b7a6e3fec990d1a6cdabc72e417
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-07 18:45:04 +1000

    Time: 60 ms (23.45%), Space: 34.2 MB (97.06%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
-        for i in range(n):
-            if i > 0:
-                nums[i] += nums[i - 1]
-            nums[i] %= k
+        nums[0] %= k
         seen = {0: True}
         for i in range(1, n):
+            nums[i] = (nums[i - 1] + nums[i]) % k
             if nums[i] in seen:
                 return True
             seen[nums[i - 1]] = True
