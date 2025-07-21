6360ad826a2010ed195cc2e140406e18e49b2608
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-21 22:12:29 +1000

    Time: 357 ms (42.35%), Space: 19.4 MB (76.94%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -2,6 +2,7 @@ class Solution:
     def makeFancyString(self, s: str) -> str:
         res = ""
         for x in s:
-            if len(res) <= 1 or res[-1] != res[-2] or res[-1] != x:
-                res += x
+            if len(res) >= 2 and res[-1] == res[-2] and res[-1] == x:
+                continue
+            res += x
         return res
