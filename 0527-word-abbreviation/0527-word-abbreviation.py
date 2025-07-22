521543ba6a406ba5fd6a98fef74785bd15911611
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-22 14:20:24 +1000

    Time: 15 ms (88.43%), Space: 19.9 MB (44.63%) - LeetHub

---
+++ 0527-word-abbreviation/0527-word-abbreviation.py
@@ -0,0 +1,38 @@
+class Solution:
+    def wordsAbbreviation(self, words: List[str]) -> List[str]:
+        n = len(words)
+
+        def abb(s: str, k: int) -> str:
+            if len(s) == k + 1:
+                return s
+            return s[:k] + f"{len(s) - k - 1}" + s[-1]
+
+        def solve(idxs, k: int):
+            s = {}
+            for idx in idxs:
+                word = abb(words[idx], k)
+                if word not in s:
+                    s[word] = []
+                s[word].append(idx)
+            res = {}
+            ans = []
+            for word in s:
+                if len(s[word]) == 1:
+                    res[word] = s[word][0]
+                else:
+                    ans.append(solve(s[word], k + 1))
+            # print(ans)
+            for i in range(len(ans)):
+                for word in ans[i]:
+                    assert word not in res
+                    res[word] = ans[i][word]
+            return res
+        
+        res = solve([x for x in range(n)], 1)
+        ans = [""] * n
+        for word in res:
+            ans[res[word]] = word
+        for i in range(n):
+            if len(ans[i]) == len(words[i]):
+                ans[i] = words[i]
+        return ans
