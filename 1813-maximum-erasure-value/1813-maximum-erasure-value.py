4f7c398a9b46b5d982447b6b6dc9665827a13da4
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-22 10:54:50 +1000

    Time: 289 ms (25.48%), Space: 28.8 MB (50.6%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        # time: O(N)
+        # space: O(N)
+        # method: sliding window (2 pointers)
+        le = 0
+        n = len(nums)
+        cnt = defaultdict(int)
+        cur, res = 0, 0
+        for ri in range(n):
+            cnt[nums[ri]] += 1
+            cur += nums[ri]
+            while le < n and cnt[nums[ri]] > 1:
+                cur -= nums[le]
+                cnt[nums[le]] -= 1
+                le += 1
+            # ri - le + 1
+            res = max(res, cur)
+        return res
