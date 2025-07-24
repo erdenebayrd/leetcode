c4125de6e7dd93f1259af14d35973370cfe46956
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-24 15:08:40 +1000

    Time: 2094 ms (38.46%), Space: 38.2 MB (46.15%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.py
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.py
@@ -6,15 +6,12 @@ class Helper:
         self.levels = [-1] * self.n
         self.xor = [0] * self.n
         self.adj = [[] for _ in range(self.n)]
+        self.subTreeContains = {}
         for u, v in edges:
             self.adj[u].append(v)
             self.adj[v].append(u)
-        self.parents = [[-1 for _ in range(11)] for _ in range(self.n)]
         self.dfs(self.root, 0, -1)
-        self.initLCA()
         self.initXOR(self.root, -1)
-        # print(self.levels)
-        # print(self.parents)
 
     def initXOR(self, cur: int, parent: int) -> None:
         self.xor[cur] = self.nums[cur]
@@ -24,44 +21,20 @@ class Helper:
             self.initXOR(child, cur)
             self.xor[cur] ^= self.xor[child]
 
-    def dfs(self, cur: int, deep: int, parent: int) -> None:
+    def dfs(self, cur: int, deep: int, parent: int) -> List[int]:
         self.levels[cur] = deep
-        self.parents[cur][0] = parent
+        self.subTreeContains[cur] = {}
+        self.subTreeContains[cur][cur] = True
+        children = [cur]
         for child in self.adj[cur]:
             if child == parent:
                 continue
-            self.dfs(child, deep + 1, cur)
-        
-    def initLCA(self) -> None:
-        for parentLevel in range(1, 11):
-            for node in range(self.n):
-                prevParent = self.parents[node][parentLevel - 1]
-                if prevParent == -1:
-                    continue
-                self.parents[node][parentLevel] = self.parents[prevParent][parentLevel - 1]
+            curChildren = self.dfs(child, deep + 1, cur)
+            for x in curChildren:
+                self.subTreeContains[cur][x] = True
+            children.extend(curChildren)
+        return children
 
-    def getLCA(self, u: int, v: int) -> int:
-        if self.levels[u] > self.levels[v]: # swap
-            w = u
-            u = v
-            v = w
-
-        while self.levels[u] != self.levels[v]:
-            diff = self.levels[v] - self.levels[u]
-            parentLevel = int(log(diff, 2))
-            v = self.parents[v][parentLevel]
-        
-        if u == v:
-            return v
-        
-        while self.parents[u][0] != self.parents[v][0]:
-            for i in range(10, -1, -1):
-                if self.parents[u][i] != self.parents[v][i]:
-                    u = self.parents[u][i]
-                    v = self.parents[v][i]
-                    break
-        return self.parents[v][0]
-    
     def getDiff(self, u: int, v: int) -> int:
         if self.levels[u] > self.levels[v]:
             w = u
@@ -70,7 +43,8 @@ class Helper:
         uXor = self.xor[u]
         vXor = self.xor[v]
         totalXor = self.xor[self.root]
-        if self.getLCA(u, v) == u: # subtree U contains V
+        # if self.getLCA(u, v) == u: # subtree U contains V
+        if v in self.subTreeContains[u]: # subtree U contains V
             arr = [totalXor ^ uXor, uXor ^ vXor, vXor]    
         else: # U and V are not included each other
             arr = [totalXor ^ uXor ^ vXor, uXor, vXor]
@@ -84,6 +58,7 @@ class Solution:
         root = 0
         helper = Helper(root, edges, nums)
         # print(helper.xor)
+        # print(helper.subTreeContains)
         res = int(1e8)
         for i in range(m):
             u, v = edges[i]
