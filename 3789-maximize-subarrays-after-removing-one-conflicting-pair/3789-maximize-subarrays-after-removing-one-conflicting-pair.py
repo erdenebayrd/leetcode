e5f0ac3459f3cfe7c79e30ae87ac0db4a98e81be
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-26 13:35:40 +1000

    Time: 711 ms (63.96%), Space: 85.3 MB (93.69%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.py
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -1,11 +1,6 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
-        conflictingPairs = [[l, r] if l < r else [r, l] for l, r in conflictingPairs]
-        conflictingPairs.sort(key=lambda x: x[1])
-        # print(conflictingPairs)
-        # sl = SortedList([])
+        conflictingPairs = sorted([[l, r] if l < r else [r, l] for l, r in conflictingPairs], key=lambda x: x[1])
         sl = [0, 0]
         m = len(conflictingPairs)
         idx = 0
@@ -13,7 +8,6 @@ class Solution:
         vote = [0] * (n + 1)
         for r in range(1, n + 1):
             while idx < m and conflictingPairs[idx][1] <= r:
-                # sl.add(conflictingPairs[idx][0])
                 sl.append(conflictingPairs[idx][0])
                 idx += 1
                 sl.sort()
@@ -21,14 +15,4 @@ class Solution:
                     sl.pop(0)
             base += r - sl[-1]
             vote[sl[-1]] += sl[-1] - sl[-2]
-            # if len(sl) == 0:
-            #     base += r
-            # elif len(sl) == 1:
-            #     base += r - sl[-1]
-            #     vote[sl[-1]] += sl[-1]
-            # else: # len(sl) >= 2
-            #     base += r - sl[-1]
-            #     vote[sl[-1]] += sl[-1] - sl[-2]
-        # print(base)
-        # print(vote)
         return base + max(vote)
