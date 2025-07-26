9763fbaacf1db20b8760c195112f7bb935a3a9ee
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-26 13:25:38 +1000

    Time: 2129 ms (21.62%), Space: 87.9 MB (41.44%) - LeetHub

---
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -0,0 +1,27 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
+        conflictingPairs = [[l, r] if l < r else [r, l] for l, r in conflictingPairs]
+        conflictingPairs.sort(key=lambda x: x[1])
+        # print(conflictingPairs)
+        sl = SortedList([])
+        m = len(conflictingPairs)
+        idx = 0
+        base = 0
+        vote = [0] * (n + 1)
+        for r in range(1, n + 1):
+            while idx < m and conflictingPairs[idx][1] <= r:
+                sl.add(conflictingPairs[idx][0])
+                idx += 1
+            if len(sl) == 0:
+                base += r
+            elif len(sl) == 1:
+                base += r - sl[-1]
+                vote[sl[-1]] += sl[-1]
+            else: # len(sl) >= 2
+                base += r - sl[-1]
+                vote[sl[-1]] += sl[-1] - sl[-2]
+        # print(base)
+        # print(vote)
+        return base + max(vote)
