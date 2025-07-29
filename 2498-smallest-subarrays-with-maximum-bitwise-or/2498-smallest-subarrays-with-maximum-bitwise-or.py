ed46458f453e591f0110178aaa88c0705d213ae2
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-07-29 10:32:05 +1000

    Time: 3218 ms (5.51%), Space: 104.6 MB (5.51%) - LeetHub

---
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.py
@@ -0,0 +1,30 @@
+class Solution:
+    def toBinary32(self, x: int) -> List[int]:
+        res = []
+        for i in range(32):
+            res.append((x >> i) & 1)
+        return res[::-1]
+
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        arr = []
+        dp = []
+        for x in nums:
+            arr.append(self.toBinary32(x))
+            dp.append([-1] * 32)
+        n = len(nums)
+        for i in range(32):
+            dp[-1][i] = n - 1
+        res = [1]
+        for i in range(n - 2, -1, -1):
+            cur = i
+            for j in range(32):
+                arr[i][j] += arr[i + 1][j]
+                if arr[i][j] > arr[i + 1][j]:
+                    dp[i][j] = i
+                else:
+                    dp[i][j] = dp[i + 1][j]
+                if arr[i][j] == 0:
+                    continue
+                cur = max(cur, dp[i][j])
+            res.append(cur - i + 1)
+        return res[::-1]
