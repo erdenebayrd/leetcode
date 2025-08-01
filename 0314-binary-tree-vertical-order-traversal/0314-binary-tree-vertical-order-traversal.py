e7c587cec4e0299a5e1081da5b7c427dacbb62bb
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-01 13:51:47 +1000

    Time: 0 ms (100%), Space: 17.8 MB (66.72%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -6,25 +6,24 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
-            return []
-        # left, level, idx, val
-        arr = []
-        idx = 0
-        def dfs(cur, left, level):
-            nonlocal idx
-            arr.append((left, level, idx, cur.val))
-            idx += 1
-            if cur.left is not None:
-                dfs(cur.left, left - 1, level + 1)
-            if cur.right is not None:
-                dfs(cur.right, left + 1, level + 1)
-        dfs(root, 0, 0)
-        arr.sort()
-        group = defaultdict(list)
-        for left, _, _, val in arr:
-            group[left].append(val)
-        res = []
-        for key in group:
-            res.append(group[key])
-        return res
+        res = {}
+        cnt = 0
+        def solve(colNum: int, cur: Optional[TreeNode], lvl: int):
+            if not cur:
+                return
+            nonlocal res
+            nonlocal cnt
+            if colNum not in res:
+                res[colNum] = []
+            res[colNum].append((lvl, cnt, cur.val))
+            cnt += 1
+            solve(colNum - 1, cur.left, lvl + 1)
+            solve(colNum + 1, cur.right, lvl + 1)
+        solve(0, root, 0)
+        answer = []
+        for _, val in sorted(res.items()):
+            cur = []
+            for _, _, num in sorted(val):
+                cur.append(num)
+            answer.append(cur)
+        return answer
