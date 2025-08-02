233b43b1abf99e184d68bb01743a69a0f2337faa
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-02 20:56:09 +1000

    Time: 403 ms (38.4%), Space: 33.6 MB (85.27%) - LeetHub

--- 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
+++ 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        n = len(rides)
         dp = [0] * (n + 1)
+        rides.append([0, 0, 0])
         rides.sort(key=lambda x: x[1])
-        rideIdx = 0
-        for ri in range(1, n + 1):
-            dp[ri] = max(dp[ri], dp[ri - 1])
-            while rideIdx < len(rides) and ri == rides[rideIdx][1]:
-                st, ed, tip = rides[rideIdx]
-                dp[ri] = max(dp[ri], dp[st] + ed - st + tip)
-                rideIdx += 1
+        for i in range(1, n + 1):
+            st, ed, tip = rides[i]
+            profit = tip + ed - st
+            lo, hi = 0, i
+            while lo + 1 < hi:
+                md = (lo + hi) // 2
+                if rides[md][1] <= st:
+                    lo = md
+                else:
+                    hi = md
+            dp[i] = max(dp[i - 1], dp[lo] + profit)
         return dp[n]
