d7510c7e2141a7c6fc7944e178341743c7f3823e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-02 12:04:49 +1000

    Time: 123 ms (9.74%), Space: 47.2 MB (42.21%) - LeetHub

---
+++ 2689-rearranging-fruits/2689-rearranging-fruits.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCost(self, basket1: List[int], basket2: List[int]) -> int:
+        cnt1 = Counter(basket1)
+        cnt2 = Counter(basket2)
+        cnt = Counter(basket1)
+        for x in basket2:
+            cnt[x] += 1
+        for cost in cnt:
+            if cnt[cost] & 1:
+                return -1
+            cnt[cost] //= 2
+        res = 0
+        extra1 = []
+        for x in cnt1:
+            y = cnt1[x] - cnt[x]
+            if y > 0:
+                extra1.extend([x] * y)
+        extra2 = []
+        for x in cnt2:
+            y = cnt2[x] - cnt[x]
+            if y > 0:
+                extra2.extend([x] * y)
+        assert len(extra1) == len(extra2)
+        extra = []
+        extra.extend(extra1)
+        extra.extend(extra2)
+        extra.sort()
+        m = len(extra) // 2
+        x = min(min(basket1), min(basket2))
+        res = 0
+        for val in extra[:m]:
+            res += min(x * 2, val)
+        return res
