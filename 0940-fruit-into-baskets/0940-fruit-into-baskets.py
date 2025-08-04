5276941bff4db9c479ce5852589ed08ab8736dd3
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-04 10:39:52 +1000

    Time: 237 ms (11.26%), Space: 23.6 MB (42.94%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        le = 0
+        uniqueCount = 0
+        cnt = {}
+        res = 0
+        for fruit in fruits:
+            if fruit not in cnt:
+                cnt[fruit] = 0
+                uniqueCount += 1
+            cnt[fruit] += 1
+            while uniqueCount > 2 and le < len(fruits):
+                assert fruits[le] in cnt
+                cnt[fruits[le]] -= 1
+                if cnt[fruits[le]] == 0:
+                    del cnt[fruits[le]]
+                    uniqueCount -= 1
+                le += 1
+            cur = 0
+            for key in cnt: # at most 2 keys
+                cur += cnt[key]
+            res = max(res, cur)
+        return res
