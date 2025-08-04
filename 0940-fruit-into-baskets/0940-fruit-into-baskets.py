67d8792e2d2beab83816c82de67dcb2966ad7a7c
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-04 10:44:25 +1000

    Time: 231 ms (12.44%), Space: 23.7 MB (42.94%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -5,15 +5,17 @@ class Solution:
         # method: sliding window
         le = 0
         uniqueCount = 0
-        cnt = defaultdict(int)
+        cnt = {}
         res = 0
         for fruit in fruits:
-            cnt[fruit] += 1
-            if cnt[fruit] == 1:
+            if fruit not in cnt:
+                cnt[fruit] = 0
                 uniqueCount += 1
+            cnt[fruit] += 1
             while uniqueCount > 2:
                 cnt[fruits[le]] -= 1
                 if cnt[fruits[le]] == 0:
+                    cnt.pop(fruits[le])
                     uniqueCount -= 1
                 le += 1
             res = max(res, sum(cnt.values()))
