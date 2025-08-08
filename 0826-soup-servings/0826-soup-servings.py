b30428180ebaf7be9a889155bbc7d22e7843b8db
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-08 11:33:00 +1000

    Time: 7 ms (93.07%), Space: 19.5 MB (56.11%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.py
+++ 0826-soup-servings/0826-soup-servings.py
@@ -1,31 +1,20 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n == 0:
-            return 0.5
-        if n >= 4800:
+        if n > 4800:
             return 1.0
-        arr = [(100, 0), (75, 25), (50, 50), (25, 75)]
-        @cache
-        def solve(a: int, b: int, p: float) -> float:
-            if a <= 0 and b > 0:
-                return p
-            if a <= 0 or b <= 0:
-                return 0
-            res = 0
-            for x, y in arr:
-                res += solve(a - x, b - y, p * 0.25)
-            return res
-        res = solve(n, n, 1)
-        @cache
-        def solve2(a: int, b: int, p: float) -> float:
-            if a <= 0 and b <= 0:
-                return p
-            if a <= 0 or b <= 0:
-                return 0
-            res = 0
-            for x, y in arr:
-                res += solve2(a - x, b - y, p * 0.25)
-            return res
+        n = (n + 24) // 25
         
-        res += solve2(n, n, 1) * 0.5
-        return res
+        @cache
+        def solve(a: int, b: int) -> float:
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0:
+                return 1
+            if b <= 0:
+                return 0
+            res = 0
+            for i in range(1, 5):
+                res += solve(a - i, b - 4 + i)
+            return res * 0.25
+
+        return solve(n, n)
