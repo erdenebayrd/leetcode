b1ed7f94418edc0dc946cdd959fb7cb8616e0d30
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-09 11:15:13 +1000

    Time: 52 ms (19.31%), Space: 39.7 MB (82.24%) - LeetHub

--- 1143-find-smallest-common-element-in-all-rows/1143-find-smallest-common-element-in-all-rows.py
+++ 1143-find-smallest-common-element-in-all-rows/1143-find-smallest-common-element-in-all-rows.py
@@ -1,33 +1,12 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
         # time: O(N ^ 2)
-        # space: O(N ^ 2) to O(N)
+        # space: O(N)
         # method: n pointers
-        n = len(mat)
-        m = len(mat[0])
-        pointers = [0] * n
-        
-        def check() -> bool: # O(N)
-            x = mat[0][pointers[0]]
-            for row, idx in enumerate(pointers):
-                if mat[row][idx] != x:
-                    return False
-            return True
-        
-        def getMax() -> int: # O(N)
-            x = mat[0][pointers[0]]
-            for row, idx in enumerate(pointers):
-                x = max(x, mat[row][idx])
-            return x
-
-        while True:
-            if check() is True: # O(N)
-                return mat[0][pointers[0]]
-            x = getMax() # O(N)
-            for i in range(n): # O(N ^ 2)
-                idx = pointers[i]
-                while idx < m and mat[i][idx] < x:
-                    idx += 1
-                pointers[i] = idx
-            if max(pointers) >= m:
-                return -1
+        cnt = defaultdict(int)
+        for row in mat:
+            for val in row:
+                cnt[val] += 1
+                if cnt[val] == len(mat):
+                    return val
+        return -1
