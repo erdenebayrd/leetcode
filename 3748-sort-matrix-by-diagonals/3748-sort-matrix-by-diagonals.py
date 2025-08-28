dc12f5e4c7a4a278493b9142b6060cd7087b9385
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-08-28 15:54:41 +1000

    Time: 15 ms (37.92%), Space: 17.7 MB (97.5%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        def getSortedArr(x: int, y: int) -> List[int]:
+            cur = []
+            while x < n and y < n:
+                cur.append(grid[x][y])
+                x += 1
+                y += 1
+            return sorted(cur)
+        
+        def replaceGrid(x: int, y: int, arr: List[int]) -> None:
+            curIdx = 0
+            while x < n and y < n and curIdx < len(arr):
+                grid[x][y] = arr[curIdx]
+                curIdx += 1
+                x += 1
+                y += 1
+
+        for i in range(n):
+            cur = getSortedArr(i, 0)
+            replaceGrid(i, 0, cur[::-1])
+        for i in range(1, n):
+            cur = getSortedArr(0, i)
+            replaceGrid(0, i, cur)
+        return grid
