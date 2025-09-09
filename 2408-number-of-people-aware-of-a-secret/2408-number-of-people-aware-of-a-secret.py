025528232cd0b1b938a40825e1192afd0e541e17
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-09-09 11:45:39 +1000

    Time: 220 ms (24.82%), Space: 17.9 MB (71.53%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
@@ -2,9 +2,10 @@ class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         dp = [0] * n
         dp[0] = 1
+        mod = int(1e9 + 7)
         # dp[i] = dp[i - forget] + dp[i - forget + 1] ... + dp[i - delay]
         # dp[i] = sum(dp[j]) j value would be from i - forget to i - delay
         for i in range(1, n):
             for j in range(max(i - forget + 1, 0), max(i - delay + 1, 0), 1):
-                dp[i] += dp[j]
-        return sum(dp[n - forget: n + 1]) % int(1e9 + 7)
+                dp[i] = (dp[i] + dp[j]) % mod
+        return sum(dp[n - forget: n + 1]) % mod
