d124d25ecc7cc750b4346b51744e7c28f00896f8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-09-10 20:33:44 +1000

    Time: 35 ms (97.56%), Space: 32 MB (42.68%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.py
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.py
@@ -1,21 +1,23 @@
 class Solution:
     def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        # time: O(N * M)
+        # space: O(N * M)
         m = len(languages)
         langs = [None]
         for i in range(m):
             langs.append(set(languages[i]))
         # print(langs)
-        friends = set()
+        users = set()
         for u, v in friendships:
             if langs[u].isdisjoint(langs[v]) is False: # ignore if there is at least one element in common (they can communicate)
                 continue
-            friends.add(u)
-            friends.add(v)
-        # print(friends)
+            users.add(u)
+            users.add(v)
+        # print(users)
         res = m
         for i in range(1, n + 1): # will tech i'th language to them all who doesn't know it.
             cost = 0
-            for user in friends:
+            for user in users:
                 if i not in langs[user]:
                     cost += 1
             res = min(cost, res)
