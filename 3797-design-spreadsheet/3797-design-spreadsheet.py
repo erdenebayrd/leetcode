01409ed0ff77c9bf2fa505001ff9c7daf9306299
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-09-19 10:13:30 +1000

    Time: 126 ms (58.04%), Space: 23.7 MB (49.77%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.py
+++ 3797-design-spreadsheet/3797-design-spreadsheet.py
@@ -3,13 +3,17 @@ class Spreadsheet:
     def __init__(self, rows: int):
         self.cells = {}
 
-    def setCell(self, cell: str, value: int) -> None:
+    def initCell(self, cell: str) -> [str, str]:
         col = str(cell[0])
-        row = int(cell[1:])
+        row = str(cell[1:])
         if row not in self.cells:
             self.cells[row] = {}
         if col not in self.cells[row]:
             self.cells[row][col] = 0
+        return row, col
+
+    def setCell(self, cell: str, value: int) -> None:
+        row, col = self.initCell(cell)
         self.cells[row][col] = value
 
     def resetCell(self, cell: str) -> None:
@@ -19,12 +23,7 @@ class Spreadsheet:
         return "A" <= x[0] <= "Z"
 
     def getCellValue(self, cell: str) -> int:
-        col = str(cell[0])
-        row = int(cell[1:])
-        if row not in self.cells:
-            self.cells[row] = {}
-        if col not in self.cells[row]:
-            self.cells[row][col] = 0
+        row, col = self.initCell(cell)
         return self.cells[row][col]
 
     def getValue(self, formula: str) -> int:
