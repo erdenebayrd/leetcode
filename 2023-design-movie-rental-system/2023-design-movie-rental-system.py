5cad618065f4a43ec217347753d9af965ea6bd80
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-09-21 14:14:14 +1000

    Time: 687 ms (38.33%), Space: 131.6 MB (8.33%) - LeetHub

---
+++ 2023-design-movie-rental-system/2023-design-movie-rental-system.py
@@ -0,0 +1,43 @@
+from sortedcontainers import SortedList
+
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        self.movies = {}
+        self.rented = SortedList([])
+        self.prices = {}
+        for shop, movie, price in entries:
+            if movie not in self.movies:
+                self.movies[movie] = SortedList([])
+            if shop not in self.prices:
+                self.prices[shop] = {}
+            if movie not in self.prices[shop]:
+                self.prices[shop][movie] = -1
+            self.prices[shop][movie] = price
+            self.movies[movie].add([price, shop])
+
+    def search(self, movie: int) -> List[int]:
+        if movie not in self.movies:
+            self.movies[movie] = SortedList([])
+        return [shop for _, shop in self.movies[movie][:5]]
+
+    def rent(self, shop: int, movie: int) -> None:
+        price = self.prices[shop][movie]
+        self.movies[movie].remove([price, shop])
+        self.rented.add([price, shop, movie])
+
+    def drop(self, shop: int, movie: int) -> None:
+        price = self.prices[shop][movie]
+        self.rented.remove([price, shop, movie])
+        self.movies[movie].add([price, shop])
+
+    def report(self) -> List[List[int]]:
+        return [[shop, movie] for _, shop, movie in self.rented[:5]]
+
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
