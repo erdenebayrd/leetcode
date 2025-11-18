a11030ddb7743956ad542d84def90b616ed3206f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-11-18 12:33:46 +1100

    Time: 542 ms (57.46%), Space: 18 MB (37.23%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,10 +1,15 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        nums.sort()
         res = 0
         n = len(nums)
-        sl = SortedList(nums)
-        for md in range(n):
-            sl.remove(nums[md])
-            for le in range(md):
-                res += sl.bisect_left(target - nums[le] - nums[md])
+        for le in range(n):
+            md = le + 1
+            ri = n - 1
+            while md < ri:
+                if nums[le] + nums[md] + nums[ri] < target:
+                    res += ri - md # (le, md, ri), (le, md, ri - 1), (le, md, ri - 2), ... , (le, md, md + 1) # all these triplets could be okay
+                    md += 1
+                else:
+                    ri -= 1
         return res
