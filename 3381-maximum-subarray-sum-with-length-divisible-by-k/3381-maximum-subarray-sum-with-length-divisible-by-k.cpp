94361de3a74ff4629cef797cfb77a03910391e7e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-11-27 11:45:28 +1100

    Time: 11 ms (66.89%), Space: 184.3 MB (40.61%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,21 +1,24 @@
 class Solution {
+private:
+    vector<long long> prefixSum;
+    long long rangeSum(int l, int r) {
+        if (l == 0) return prefixSum[r];
+        return prefixSum[r] - prefixSum[l - 1];
+    }
+
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix_sum(n, 0);
-        prefix_sum[0] = nums[0];
-        for (int i = 1; i < n; i++) prefix_sum[i] += prefix_sum[i - 1] + nums[i];
-        function <long long(int, int)> range_sum = [&](int l, int r) {
-            if (l == 0) return prefix_sum[r];
-            return prefix_sum[r] - prefix_sum[l - 1];
-        };
+        prefixSum.assign(n, 0);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) prefixSum[i] += prefixSum[i - 1] + nums[i];
         const long long inf = (long long)(2e14);
         vector<long long> dp(n, -inf);
-        dp[k - 1] = range_sum(0, k - 1);
+        dp[k - 1] = rangeSum(0, k - 1);
         for (int i = k; i < n; i++) {
-            long long k_sum = range_sum(i - k + 1, i);
-            // cout << i - k + 1 << " " << i << " " << k_sum << endl;
-            dp[i] = max(k_sum, k_sum + dp[i - k]);
+            long long kSum = rangeSum(i - k + 1, i);
+            // cout << i - k + 1 << " " << i << " " << kSum << endl;
+            dp[i] = max(kSum, kSum + dp[i - k]);
         }
         // for (int i = 0; i < n; i++) cout << dp[i] << " "; cout << endl;
         return *max_element(dp.begin(), dp.end());
