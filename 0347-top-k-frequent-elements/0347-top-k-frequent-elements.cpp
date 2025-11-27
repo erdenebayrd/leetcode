a1838743a31b95d7443bed4be3acc9f98f869c9e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-11-28 07:50:20 +1100

    Time: 0 ms (100%), Space: 18.2 MB (28.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int, int> ump;
+        const int N = int(1e4);
+        int cnt[int(2e4) + 1];
+        memset(cnt, 0, sizeof cnt);
         unordered_map<int, vector<int>> rev;
-        for (int i = 0; i < n; i++) ump[nums[i]] += 1;
+        for (int i = 0; i < n; i++) cnt[nums[i] + N] += 1;
         vector<int> freq(n + 1, 0);
-        for (auto x: ump) {
-            rev[x.second].push_back(x.first);
-            freq[x.second] += 1;
+        for (int i = 0; i <= int(2e4); i++) {
+            if (cnt[i] == 0) continue;
+            rev[cnt[i]].push_back(i - N);
+            freq[cnt[i]] += 1;
         }
         vector<int> res;
-        // count sort O(N)
         for (int i = n; k > 0 && i > 0; i--) {
             int idx = 0;
             while (k > 0 && freq[i] > 0) {
