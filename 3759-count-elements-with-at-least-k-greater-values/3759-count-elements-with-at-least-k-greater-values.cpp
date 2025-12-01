e2c995383e1b95118f5ca2135f5795ff6a02aa67
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-01 13:30:07 +1100

    Time: 324 ms (35%), Space: 253 MB (5%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
+        unordered_map<int, int> pos;
+        for (int i = 0; i < n; i++) pos[nums[i]] = i;
         int res = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int idx = prev(upper_bound(nums.begin(), nums.end(), nums[i])) - nums.begin();
-            res += nums.size() - 1 - idx >= k;
+        for (int i = 0; i < n; i++) {
+            int cnt = n - pos[nums[i]] - 1;
+            if (cnt >= k) res += 1;
         }
         return res;
     }
