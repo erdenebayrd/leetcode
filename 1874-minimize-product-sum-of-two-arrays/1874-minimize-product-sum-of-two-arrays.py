590e483af3a0f127d5b471906ad15c828794ff08
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-08 12:10:53 +1100

    Time: 882 ms (27.87%), Space: 22.5 MB (86.89%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
@@ -1,8 +1,22 @@
 class Solution:
     def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort(reverse=True)
+        n = len(nums1)
+        cnt1 = [0] * 101
+        cnt2 = [0] * 101
+        for i in range(n):
+            cnt1[nums1[i]] += 1
+            cnt2[nums2[i]] += 1
+        i = 1
+        j = 100
         res = 0
-        for i in range(len(nums1)):
-            res += nums1[i] * nums2[i]
+        while i <= 100 and j >= 1:
+            while i <= 100 and cnt1[i] == 0:
+                i += 1
+            while j >= 1 and cnt2[j] == 0:
+                j -= 1
+            if n > 0:
+                res += i * j
+                cnt1[i] -= 1
+                cnt2[j] -= 1
+                n -= 1
         return res
