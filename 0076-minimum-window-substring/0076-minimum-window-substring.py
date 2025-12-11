f2a241dabc6aab05258493ce9b671c5a36622d9f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-11 19:30:32 +1100

    Time: 152 ms (23.62%), Space: 18.3 MB (54%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -29,10 +29,10 @@ class Solution:
                     le -= 1
                     m = add(s[le], m)
                     assert m == 0
+                    if res[-1] == -1 or res[1] - res[0] > ri - le:
+                        res = [le, ri]
                     break
-            if m == 0:
-                if res[-1] == -1 or res[1] - res[0] > ri - le:
-                    res = [le, ri]
+                
         if res[-1] == -1:
             return ""
         return s[res[0]: res[1] + 1]
