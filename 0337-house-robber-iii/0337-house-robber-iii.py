fa000ca0977d1826a7eab19c1e089af20927477a
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-16 18:19:34 +1100

    Time: 7 ms (35.85%), Space: 21.8 MB (12.02%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -8,14 +8,10 @@ class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         @cache
         def calc(cur: Optional[TreeNode]) -> [int, int]:
-            if cur.left is None and cur.right is None:
-                return [0, cur.val]
-            leftVal = [0, 0]
-            rightVal = [0, 0]
-            if cur.left is not None:
-                leftVal = calc(cur.left)
-            if cur.right is not None:
-                rightVal = calc(cur.right)
+            if not cur:
+                return [0, 0]
+            leftVal = calc(cur.left)
+            rightVal = calc(cur.right)
             return [max(leftVal) + max(rightVal), cur.val + leftVal[0] + rightVal[0]]
         
         return max(calc(root))
