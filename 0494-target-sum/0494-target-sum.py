2df69e1ff5a60e7da716a95e5ffa378cff263921
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-16 17:33:53 +1100

    Time: 107 ms (69.67%), Space: 18.1 MB (68.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        @cache
-        def solve(idx: int, cur: int) -> int:
-            if idx >= n:
-                return int(cur == target)
-            res = solve(idx + 1, cur - nums[idx])
-            res += solve(idx + 1, cur + nums[idx])
-            return res
-        
-        return solve(0, 0)
+        cnt = defaultdict(int) # how many different ways to create keys inside it
+        cnt[0] = 1 # initialize value
+        for x in nums:
+            cur = defaultdict(int)
+            for key, val in cnt.items():
+                cur[key - x] += val
+                cur[key + x] += val
+            cnt = cur
+        return cnt[target]
