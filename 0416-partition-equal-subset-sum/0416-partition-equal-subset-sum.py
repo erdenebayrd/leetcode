17503a1d649b75ff6ca3435ad625587da013ba08
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-16 17:09:03 +1100

    Time: 9217 ms (5.03%), Space: 20.4 MB (45.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # time: O(N * M), M is sum(nums)
-        # space: O(N * M), M is sum(nums)
-        # method: bottom up DP
-        n = len(nums)
-        m = sum(nums)
-        if m & 1:
+        s = sum(nums)
+        if s & 1:
             return False
-        m //= 2
-        dp = [[False] * (m + 1) for _ in range(n + 1)]
-        dp[0][0] = True
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                dp[i][j] = dp[i - 1][j]
-                if j - nums[i - 1] >= 0:
-                    dp[i][j] |= dp[i - 1][j - nums[i - 1]]
-        return dp[n][m]
+
+        pre = defaultdict(int)
+        for x in nums:
+            cur = deepcopy(pre)
+            for key in pre:
+                cur[key + x] = 1
+            cur[x] = 1
+            pre = deepcopy(cur)
+        return pre[s // 2] == 1
