706c76a65b8ab4832fe687b22f03b3c2b0776c23
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-17 18:41:02 +1100

    Time: 4380 ms (15.61%), Space: 199.1 MB (5.14%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -1,34 +1,30 @@
 class Solution:
-    def maximumProfit(self, prices: List[int], m: int) -> int:
-        # time: O(k * n)
-        # space: O(k * n)
-        
-        # m pairs, which need to choose at most without any intersection between them
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        # time: O(k * n * 3)
+        # space: O(k * n * 3)
         n = len(prices)
-        dp = [[0] * n for _ in range(m + 1)]
-        for i in range(1, m + 1):
-            maxLong = -prices[0]   # k=-1 case: dp[i-1][-1]=0, buy at index 0
-            maxShort = prices[0]   # k=-1 case: dp[i-1][-1]=0, short at index 0
-            for j in range(1, n):
-                dp[i][j] = dp[i][j - 1]
+        inf = int(1e12 + 1)
 
-                # end transaction at j
-                dp[i][j] = max(dp[i][j], prices[j] + maxLong)    # sell long
-                dp[i][j] = max(dp[i][j], maxShort - prices[j])   # cover short
-                # for k in range(j - 1):
-                #     dp[i][j] = max(dp[i][j], dp[i - 1][k] + abs(prices[k + 1] - prices[j]))
-                # # The `abs(prices[k+1] - prices[j])` can be split into two cases:
-                # # - **Long (buy then sell):** `prices[j] - prices[k+1]`
-                # # - **Short (sell then buy):** `prices[k+1] - prices[j]`
-
-                # if i == 1:
-                #     dp[i][j] = max(dp[i][j], abs(prices[0] - prices[j]))
-                # at most, not exactly M, can be lower than M and can be exactly M
-                dp[i][j] = max(dp[i][j], dp[i - 1][j])
-
-                # update running max for next j
-                # k = j-1: can start new transaction at index j
-                maxLong = max(maxLong, dp[i - 1][j - 1] - prices[j])
-                maxShort = max(maxShort, dp[i - 1][j - 1] + prices[j])
-
-        return dp[m][n - 1]
+        @cache
+        def solve(idx: int, k: int, state: int) -> int:
+            if k < 0:
+                return -inf
+            if idx >= n:
+                if state == 0:
+                    return 0
+                return -inf
+            res = solve(idx + 1, k, state)
+            if state == 0: # not holding anything
+                res = max(res, solve(idx + 1, k - 1, 1) - prices[idx]) # LONG position started at idx
+                res = max(res, solve(idx + 1, k - 1, 2) + prices[idx]) # SHORT position started at idx
+            elif state == 1: # LONG position started before
+                res = max(res, solve(idx + 1, k, 0) + prices[idx]) # closing current LONG position
+            elif state == 2: # SHORT position started before
+                res = max(res, solve(idx + 1, k, 0) - prices[idx]) # closing current SHORT position
+            else:
+                assert False
+            return res
+        
+        res = solve(0, k, 0)
+        solve.cache_clear()
+        return res
