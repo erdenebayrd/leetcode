97f05d6a7644f06622da4945324b499d60292001
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-19 12:02:34 +1100

    Time: 581 ms (14.02%), Space: 74.2 MB (15.29%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,55 @@
+class UnionFindDisjointSet:
+    def __init__(self):
+        self.parent = {}
+    
+    def findParent(self, node: int):
+        if node not in self.parent:
+            self.parent[node] = node
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.findParent(self.parent[node])
+        return self.parent[node]
+    
+    def isSameSet(self, nodeX: int, nodeY: int) -> None:
+        parentX = self.findParent(nodeX)
+        parentY = self.findParent(nodeY)
+        return parentX == parentY
+
+    def union(self, nodeX: int, nodeY: int) -> None:
+        parentX = self.findParent(nodeX)
+        parentY = self.findParent(nodeY)
+        if parentX == parentY:
+            return
+        self.parent[parentX] = self.parent[parentY]
+
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        pool = set([0, firstPerson])
+        meetings.sort(key=lambda m: m[2]) # sort by time
+        meets = {}
+        for x, y, time in meetings:
+            if time not in meets:
+                meets[time] = []
+            meets[time].append([x, y])
+        
+        for time in meets:
+            localPool = UnionFindDisjointSet()
+            users = set()
+            for x, y in meets[time]:
+                localPool.union(x, y)
+                users.add(x)
+                users.add(y)
+            parents = {}
+            for user in users:
+                parent = localPool.findParent(user)
+                if parent not in parents:
+                    parents[parent] = set()
+                parents[parent].add(user)
+            for parent in parents:
+                flag = False
+                for user in parents[parent]:
+                    flag |= user in pool
+                if flag is True:
+                    for user in parents[parent]:
+                        pool.add(user)
+        return list(pool)
