23ca5708bd9c1c7da36e5ecf93356370eb70383f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2025-12-22 23:19:51 +1100

    Time: 997 ms (5.33%), Space: 18.2 MB (28%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
@@ -1,7 +1,8 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        # time: O(M * M * N) -> O(N^3)
-        # space: O(N ^ 3)
+        # time: O(N ^ 3)
+        # space: O(N ^ 2)
+        
         n = len(strs)
         for i in range(n):
             strs[i] = "a" + strs[i]
@@ -15,6 +16,7 @@ class Solution:
             res = 1 + solve(le, ri + 1) 
 
             # Case 2: don't delete ri'th row if and only if all rows individualy sorted by lexographic
+            # otherwise ri'th row must be deleted and this case is covered by Case 1
             if all([strs[i][le] <= strs[i][ri] for i in range(n)]):
                 res = min(res, solve(ri, ri + 1))
             
