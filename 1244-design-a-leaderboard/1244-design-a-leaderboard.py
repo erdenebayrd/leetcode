ffec7e370c9a75181b8d509f48d5778650027519
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-01 22:21:27 +1100

    Time: 19 ms (57.9%), Space: 17.5 MB (99.09%) - LeetHub

---
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -0,0 +1,28 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.arr = []
+
+    def addScore(self, playerId: int, score: int) -> None:
+        found = False
+        for i in range(len(self.arr)):
+            if playerId == self.arr[i][1]:
+                self.arr[i][0] += score
+                found = True
+        if found is False:
+            self.arr.append([score, playerId])
+
+    def top(self, K: int) -> int:
+        self.arr.sort(reverse=True)
+        return sum([x for x, _ in self.arr[:K]])
+
+    def reset(self, playerId: int) -> None:
+        for i in range(len(self.arr)):
+            if self.arr[i][1] == playerId:
+                self.arr[i][0] = 0
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
