38ea0383c69b181b3784fccb54275287521610a6
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-01 23:14:37 +1100

    Time: 35 ms (14.25%), Space: 21.1 MB (6.22%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,25 +1,57 @@
+class FenwickTree:
+    def __init__(self, sz: int) -> None:
+        self.ft = [0] * (sz + 1)
+    
+    def update(self, idx: int, value: int) -> None:
+        while idx > 0 and idx < len(self.ft):
+            self.ft[idx] += value
+            idx += idx & -idx
+    
+    def get(self, idx: int) -> int:
+        res = 0
+        while idx > 0:
+            res += self.ft[idx]
+            idx -= idx & -idx
+        return res
+    
+    def rangeSum(self, l: int, r: int) -> int:
+        return self.get(r) - self.get(l - 1)
+
 class Leaderboard:
 
     def __init__(self):
-        self.arr = []
+        self.cnt = FenwickTree(100000)
+        self.sum = FenwickTree(100000)
+        self.score = defaultdict(int)
+
+    def __update(self, score: int, value: int) -> None:
+        self.cnt.update(score, value) # O(Log(1e5))
+        self.sum.update(score, value * score) # O(Log(1e5))
 
     def addScore(self, playerId: int, score: int) -> None:
-        found = False
-        for i in range(len(self.arr)):
-            if playerId == self.arr[i][1]:
-                self.arr[i][0] += score
-                found = True
-        if found is False:
-            self.arr.append([score, playerId])
+        # remove
+        self.__update(self.score[playerId], -1)
+        # update
+        self.score[playerId] += score
+        # add
+        self.__update(self.score[playerId], 1)
 
     def top(self, K: int) -> int:
-        self.arr.sort()
-        return sum([x for x, _ in self.arr[-K:]])
+        n = int(1e5)
+        lo, hi = 0, n + 1
+        while lo + 1 < hi:
+            md = (lo + hi) // 2
+            if self.cnt.rangeSum(md, n) >= K: # O(Log(1e5) ^ 2)
+                lo = md
+            else:
+                hi = md
+        extra = self.cnt.rangeSum(lo, n) - K
+        return self.sum.rangeSum(lo, n) - extra * lo
 
     def reset(self, playerId: int) -> None:
-        for i in range(len(self.arr)):
-            if self.arr[i][1] == playerId:
-                self.arr[i][0] = 0
+        # remove
+        self.__update(self.score[playerId], -1)
+        self.score[playerId] = 0
 
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
