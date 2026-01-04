d09f761df78472be13b083ddb47f4d0bfef6439d
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-05 10:33:09 +1100

    Time: 159 ms (89.35%), Space: 51.1 MB (47.65%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,27 +1,20 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort()
-        startAt = [0] * n
         cnt = [0] * n
-        for st, ed in meetings:
-            room = n
-            startAtMin, roomMin = int(5e10 + 1), n
-            for i in range(n):
-                if startAt[i] <= st:
-                    room = min(room, i)
-                if startAtMin > startAt[i]:
-                    startAtMin = startAt[i]
-                    roomMin = i
-            if room == n:
-                room = roomMin
-            # print(startAtMin, room)
-            start = max(startAt[room], st)
-            end = start + ed - st
-            startAt[room] = end
+        available = list(range(n)) # rooms
+        busy = [] # first value would be end time, second be room number
+        heapq.heapify(available)
+        heapq.heapify(busy)
+        for start, end in meetings:
+            while busy and busy[0][0] <= start:
+                _, room = heapq.heappop(busy)
+                heapq.heappush(available, room)
+            if available:
+                room = heapq.heappop(available)
+                heapq.heappush(busy, (end, room))
+            else:
+                endTime, room = heapq.heappop(busy)
+                heapq.heappush(busy, (endTime + (end - start), room))
             cnt[room] += 1
-        room, usage = -1, -1
-        for i in range(n):
-            if usage < cnt[i]:
-                usage = cnt[i]
-                room = i
-        return room
+        return cnt.index(max(cnt))
