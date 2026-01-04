a7fb1af7a032cb69ebf5ceac2e9f41175e147ca8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-04 20:55:25 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.py
+++ 1390-four-divisors/1390-four-divisors.py
@@ -1,26 +1,14 @@
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        n = int(1e5)
-        cnt = [0] * (n + 1)
-        for i in range(1, n + 1):
-            for j in range(i, n + 1, i):
-                cnt[j] += 1
-        arr = []
-        for i in range(1, n + 1):
-            if cnt[i] == 4:
-                arr.append([i, 0])
-        for i in range(len(arr)):
-            x, _ = arr[i]
-            cur = -1
-            for j in range(2, x):
-                if x % j == 0:
-                    cur = j
-                    break
-            arr[i][1] = 1 + cur + x // cur + x
-        cnt = defaultdict(int)
-        for x, y in arr:
-            cnt[x] = y
         res = 0
         for x in nums:
-            res += cnt[x]
+            divisors = []
+            for i in range(2, int(x ** 0.5)):
+                if x % i == 0:
+                    divisors.append(x // i)
+                    divisors.append(i)
+                if len(divisors) > 2:
+                    break
+            if len(divisors) == 2:
+                res += sum(divisors) + 1 + x
         return res
