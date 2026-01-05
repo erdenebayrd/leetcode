0629ae510406664efa623188351872b8ef643af5
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-05 12:27:07 +1100

    Time: 103 ms (36.08%), Space: 26.9 MB (6.96%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -5,8 +5,9 @@ class Solution:
         cntNegative = 0
         for row in matrix:
             for val in row:
-                mnAbs = min(mnAbs, abs(val))
-                total += abs(val)
+                x = abs(val)
+                mnAbs = min(mnAbs, x)
+                total += x
                 cntNegative += int(val < 0)
         if cntNegative & 1:
             total -= 2 * mnAbs
