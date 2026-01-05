9fffafd89da8c0a7edef62bc1baf36de10a5306d
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-05 13:30:25 +1100

    Time: 2773 ms (65.79%), Space: 34.7 MB (92.11%) - LeetHub

--- 2818-apply-operations-to-maximize-score/2818-apply-operations-to-maximize-score.py
+++ 2818-apply-operations-to-maximize-score/2818-apply-operations-to-maximize-score.py
@@ -1,102 +1,86 @@
 # My solution
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
-        def sieve(N: int) -> List[int]:
-            primes = [2]
-            arr = [True] * (N + 1)
-            for i in range(3, N + 1, 2):
-                if arr[i] is False:
+        n = len(nums)
+        primeScores = [0] * n
+        cnt = [0] * n # how many times i'th element could be chosen by highest prime score
+        mod = 1_000_000_007
+        m = max(nums) + 1
+        primes = [2]
+        def sieve():
+            isPrime = [True] * m
+            isPrime[0] = isPrime[1] = False
+            for i in range(3, m, 2):
+                if isPrime[i] is False:
                     continue
                 primes.append(i)
-                for j in range(i * i, N + 1, i):
-                    arr[j] = False
-            return primes
+                for j in range(i * i, m, i):
+                    isPrime[j] = False
+        sieve()
         
-        primes = sieve(int(1e5 ** 0.5) + 1)
-        # print(len(primes))
-
-        n = len(nums)
-        # n = int(1e5)
-        # nums = [i + 1 for i in range(n)]
-        primeScores = [0] * n # count of distinct primes
-        for i, x in enumerate(nums):
+        for i in range(n):
+            x = nums[i]
             for prime in primes:
-                if x % prime == 0:
-                    primeScores[i] += 1
+                if prime * prime > x:
+                    break
+                if x % prime != 0:
+                    continue
+                primeScores[i] += 1
                 while x % prime == 0:
-                    x //= prime
+                    x /= prime
             primeScores[i] += int(x > 1)
-        # print(max(primeScores))
-        # m = 1
-        # for i in range(max(primeScores) + 1):
-        #     m *= primes[i]
-        # print(m)
-
-        xl, xr = [], []
-        for i in range(n):
-            xl.append(i)
-            xr.append(i)
-        
-        maxPrimeScore = max(primeScores)
-        positionPrimeScores = [deque() for i in range(maxPrimeScore + 1)]
-        for i in range(n):
-            positionPrimeScores[primeScores[i]].append(i)
         # print(primeScores)
+        # left to right for leftMostIndex
+        decreasingMonotonicStack = deque()
+        leftMostIndex = [-1] * n
+        for i in range(n):
+            while decreasingMonotonicStack and primeScores[decreasingMonotonicStack[-1]] < primeScores[i]:
+                decreasingMonotonicStack.pop()
+            if decreasingMonotonicStack:
+                leftMostIndex[i] = decreasingMonotonicStack[-1]
+            decreasingMonotonicStack.append(i)
+        
+        # right to left for rightMostIndex
+        decreasingMonotonicStack = deque()
+        rightMostIndex = [n] * n
         for i in range(n - 1, -1, -1):
-            positionPrimeScores[primeScores[i]].pop() # pop right most index
-            rightMostIdx = -1
-            for j in range(primeScores[i], maxPrimeScore + 1, 1):
-                if len(positionPrimeScores[j]) > 0:
-                    rightMostIdx = max(rightMostIdx, positionPrimeScores[j][-1])
-            xl[i] = rightMostIdx + 1
-        # print(xl)
-        for i in range(n):
-            assert xl[i] <= i
+            while decreasingMonotonicStack and primeScores[decreasingMonotonicStack[-1]] <= primeScores[i]:
+                decreasingMonotonicStack.pop()
+            if decreasingMonotonicStack:
+                rightMostIndex[i] = decreasingMonotonicStack[-1]
+            decreasingMonotonicStack.append(i)
         
-        positionPrimeScores = [deque() for i in range(maxPrimeScore + 1)]
         for i in range(n):
-            positionPrimeScores[primeScores[i]].append(i)
+            cnt[i] = (i - leftMostIndex[i]) * (rightMostIndex[i] - i)
+        del rightMostIndex
+        del leftMostIndex
+        del decreasingMonotonicStack
+        del primes
+        del m
+        del primeScores
+        arr = []
         for i in range(n):
-            positionPrimeScores[primeScores[i]].popleft()
-            leftMostIndex = n
-            for j in range(primeScores[i] + 1, maxPrimeScore + 1, 1):
-                if len(positionPrimeScores[j]) > 0:
-                    leftMostIndex = min(leftMostIndex, positionPrimeScores[j][0])
-            xr[i] = leftMostIndex - 1
-        # print(xr)
-        for i in range(i):
-            assert i <= xr[i]
-        
-        mod = int(1e9 + 7)
-        x = [0] * n
-        for i in range(n):
-            le = i - xl[i] + 1
-            ri = xr[i] - i + 1
-            # x[i] = (le * ri) % (mod - 1) # fermat's little theorem -> Reduce k by x[i] not after mod - 1. Can't apply here fermat's little theorem
-            x[i] = le * ri
-        # print(x)
-        # print(max(x))
-
-        def powerLog(a: int, x: int) -> int:
-            res = 1
-            while x > 0:
-                if x & 1:
-                    res = (res * a) % mod
-                    x -= 1
-                else:
-                    x //= 2
-                    a = (a * a) % mod
-            return res
-
-        arr = [(nums[i], x[i]) for i in range(n)]
+            arr.append((nums[i], cnt[i]))
         arr.sort(reverse=True)
+        # print(arr)
+
+        def powLog(x: int, y: int) -> int:
+            cur = 1
+            while y > 0:
+                if y & 1:
+                    cur = (cur * x) % mod
+                    y -= 1
+                else:
+                    x = (x * x) % mod
+                    y //= 2
+            return cur
+        
         res = 1
         for i in range(n):
-            num, xx = arr[i]
-            power = min(xx, k)
-            res = (res * powerLog(num, power)) % mod
-            k -= power
-            if k <= 0:
+            if k == 0:
                 break
-        # print(set(primeScores))
+            y = min(k, arr[i][1])
+            x = arr[i][0]
+            res = (res * powLog(x, y)) % mod
+            k -= y
         return res
