56c60de8a3a019e4629568a422458f18d5e48405
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-01-15 13:33:40 +1100

    Time: 0 ms (100%), Space: 19.5 MB (13.85%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
@@ -1,22 +1,19 @@
 class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
-        hBars.sort()
-        vBars.sort()
-        lch = 1
-        cur = 1
-        for i in range(1, len(hBars)):
-            if hBars[i - 1] + 1 == hBars[i]:
-                cur += 1
-            else:
-                cur = 1
-            lch = max(lch, cur)
-        lcv = 1
-        cur = 1
-        for i in range(1, len(vBars)):
-            if vBars[i - 1] + 1 == vBars[i]:
-                cur += 1
-            else:
-                cur = 1
-            lcv = max(lcv, cur)
-        w = min(lcv, lch) + 1
-        return w * w
+        def max_continius_stick(arr: List[int]) -> int:
+            assert len(arr) >= 1
+            arr.sort()
+            res = 1
+            cur = 1
+            for i in range(1, len(arr)):
+                if arr[i - 1] + 1 == arr[i]:
+                    cur += 1
+                else:
+                    cur = 1
+                res = max(res, cur)
+            return res
+
+        width = max_continius_stick(vBars)
+        height = max_continius_stick(hBars)
+        area = (min(width, height) + 1) ** 2
+        return area
