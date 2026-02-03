e78145f5c713a8f4b2f259bffca05bd5776e1ecb
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-03 16:39:20 +1100

    Time: 3 ms (87.93%), Space: 19.3 MB (42.44%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -2,17 +2,14 @@ class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         n = len(nums)
         def findLastIndex(startIndex: int, isIncreasing: bool) -> int:
-            if isIncreasing is True:
-                for i in range(startIndex, n - 1):
+            for i in range(startIndex, n - 1):
+                if isIncreasing is True:
                     if nums[i] >= nums[i + 1]:
                         return i
-                return n - 1
-            else: # find strictly decreasing
-                for i in range(startIndex, n - 1):
+                else:
                     if nums[i] <= nums[i + 1]:
                         return i
-                return n - 1
-        
+            return n - 1
         p = findLastIndex(0, True)
         q = findLastIndex(p, False)
         return n - 1 == findLastIndex(q, True) and 0 < p and q < n - 1
