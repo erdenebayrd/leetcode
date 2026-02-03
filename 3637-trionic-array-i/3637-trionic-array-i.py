36c0dc0624e71ade05f3caf81400894d2cb21ea8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-03 16:40:17 +1100

    Time: 3 ms (87.93%), Space: 19.4 MB (42.44%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -3,12 +3,8 @@ class Solution:
         n = len(nums)
         def findLastIndex(startIndex: int, isIncreasing: bool) -> int:
             for i in range(startIndex, n - 1):
-                if isIncreasing is True:
-                    if nums[i] >= nums[i + 1]:
-                        return i
-                else:
-                    if nums[i] <= nums[i + 1]:
-                        return i
+                if (isIncreasing is True and nums[i] >= nums[i + 1]) or (isIncreasing is False and nums[i] <= nums[i + 1]):
+                    return i
             return n - 1
         p = findLastIndex(0, True)
         q = findLastIndex(p, False)
