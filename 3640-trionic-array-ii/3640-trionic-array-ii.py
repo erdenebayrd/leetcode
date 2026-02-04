bef019f747c4f1b5dfd820eefe41914173e954a8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-04 22:22:58 +1100

    Time: 334 ms (96.55%), Space: 33.2 MB (98.28%) - LeetHub

---
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -0,0 +1,40 @@
+from typing import Literal
+
+class Solution:
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        def farthestIndexStrictly(startIndex: int, order: Literal["ascending", "descending"], step: Literal[1, -1]) -> int:
+            stop = 0 if step == -1 else len(nums) - 1
+            for i in range(startIndex, stop, step):
+                if (order == "ascending" and nums[i] >= nums[i + step]) or (order == "descending" and nums[i] <= nums[i + step]):
+                    return i
+            return len(nums) - 1 if step == 1 else 0
+        
+        def maxSum(startIndex: int, endIndex: int) -> int:
+            step = -1 if startIndex > endIndex else 1
+            currentSum = nums[startIndex]
+            result = currentSum
+            for i in range(startIndex + step, endIndex + step, step):
+                currentSum += nums[i]
+                result = max(result, currentSum)
+            return result
+
+        res = float('-inf')
+        middlePeakIndex = 1
+        while middlePeakIndex < len(nums):
+            middleBottomIndex = farthestIndexStrictly(startIndex=middlePeakIndex, order="descending", step=1)
+            if middlePeakIndex == middleBottomIndex: # not found any decreasing subarray
+                middlePeakIndex += 1
+                continue
+            leftIndex = farthestIndexStrictly(startIndex=middlePeakIndex, order="descending", step=-1)
+            if leftIndex == middleBottomIndex: # not found any increasing subarray which end middlePeakIndex
+                middlePeakIndex = middleBottomIndex
+                continue
+            rightIndex = farthestIndexStrictly(startIndex=middleBottomIndex, order="ascending", step=1)
+            if rightIndex == middleBottomIndex: # not found any increasing subarray starting from middleBottomIndex
+                middlePeakIndex = middleBottomIndex
+                continue
+            currentSum = sum(nums[middlePeakIndex:middleBottomIndex + 1])
+            res = max(res, maxSum(middlePeakIndex - 1, leftIndex) + currentSum + maxSum(middleBottomIndex + 1, rightIndex))
+            middlePeakIndex = middleBottomIndex
+        
+        return res
