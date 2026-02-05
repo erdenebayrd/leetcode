eeac76092917ddcf581b74eaf0a8d614091ca7f9
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-05 19:02:35 +1100

    Time: 313 ms (16.43%), Space: 57.1 MB (5.09%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        fullBlooms = defaultdict(int)
+        for start, end in flowers:
+            fullBlooms[start] += 1
+            fullBlooms[end + 1] -= 1
+        for arrive in people:
+            if arrive not in fullBlooms:
+                fullBlooms[arrive] = 0
+        fullBlooms = sorted([[timestamp, flowers] for timestamp, flowers in fullBlooms.items()])
+        for i in range(1, len(fullBlooms)):
+            fullBlooms[i][1] += fullBlooms[i - 1][1]
+        
+        fullBloomsByTime = defaultdict(int)
+        for timestamp, flowers in fullBlooms:
+            fullBloomsByTime[timestamp] = flowers
+        answer = []
+        for arrive in people:
+            answer.append(fullBloomsByTime[arrive])
+        return answer
