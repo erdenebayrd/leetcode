1a73881ab907e703dfed7ca9cb4e4367252bf87f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-08 12:19:32 +1100

    Time: 3 ms (57.93%), Space: 20.5 MB (39.04%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -8,12 +8,11 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode], depth: int = 0) -> bool:
         if not root:
             return True
-        root.val = depth
         result = self.isBalanced(root.left, depth + 1) & self.isBalanced(root.right, depth + 1)
         if result is False:
             return False
-        leftDepth = root.val if root.left is None else root.left.val
-        rightDepth = root.val if root.right is None else root.right.val
+        leftDepth = depth if root.left is None else root.left.val
+        rightDepth = depth if root.right is None else root.right.val
         result &= (abs(leftDepth - rightDepth) <= 1)
         root.val = max(leftDepth, rightDepth)
         return result
