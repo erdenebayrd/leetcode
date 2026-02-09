9172b9e921c47b6aa108cba0aa125f382e654100
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-09 11:04:55 +1100

    Time: 135 ms (85.8%), Space: 20 MB (73.01%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,20 +1,12 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n = len(s)
-        suffixA = [0] * (n + 1)
-        for i in range(n - 1, -1, -1):
-            suffixA[i] += suffixA[i + 1] + int(s[i] == 'a')
-        
-        @cache
-        def solve(index: int) -> int:
-            if index >= n:
-                return 0
-            res = float('inf')
-            if s[index] == 'b':
-                res = min(res, suffixA[index])
-                res = min(res, 1 + solve(index + 1))
-            else:
-                res = min(res, solve(index + 1))
-            return res
-        
-        return solve(0)
+        # time: O(N)
+        # space: O(1)
+        result = 0
+        countB = 0
+        for ch in s:
+            if ch == 'a':
+                result = min(result + 1, countB) # means delete all occurrences of "b" before this character OR delete this character "a" then go to next
+            else: # b
+                countB += 1
+        return result
