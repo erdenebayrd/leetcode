11df6805d8181897a0783d3f3b9a829d8ffb5f58
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-10 14:21:40 +1100

    Time: 2732 ms (5.11%), Space: 19.9 MB (13.62%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            counter = defaultdict(int)
+            balanced = 0
+            for j in range(i, n):
+                counter[nums[j]] += 1
+                if counter[nums[j]] == 1:
+                    balanced += int(nums[j] % 2 == 1) - int(nums[j] % 2 == 0)
+                if balanced == 0:
+                    res = max(res, j - i + 1)
+        return res
