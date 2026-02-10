15100d85fe724d266e4d3e4609073f770eb6b1f4
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-10 12:16:28 +1100

    Time: 2055 ms (9.85%), Space: 77.6 MB (5.3%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -1,37 +1,32 @@
 class Solution:
     def minimumMoves(self, grid: List[List[int]]) -> int:
-        # time: O(factorial(8))
-        # space: O(N) N is number of cells which basically is 9 auxiliary 
-        
-        def minimumCostMatch(left: List[Tuple[int, int]], right: List[Tuple[int, int]]) -> int:
-            if len(left) != len(right):
-                raise "invalid input"
-            if len(left) == 0:
-                return 0
-            cost = float('inf')
-            nextLeft = []
-            for i in range(1, len(left)):
-                nextLeft.append(left[i])
-            for i in range(len(right)):
-                leftRow, leftColumn = left[0]
-                rightRow, rightColumn = right[i]
-                currentCost = abs(leftRow - rightRow) + abs(leftColumn - rightColumn)
-                nextRight = []
-                for j in range(len(right)):
-                    if i == j:
-                        continue
-                    nextRight.append(right[j])
-                cost = min(cost, currentCost + minimumCostMatch(nextLeft, nextRight))
-            return cost
-
-        left = []
-        right = []
+        left, right = [], []
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 0:
                     right.append([i, j])
-                elif grid[i][j] > 1:
-                    for _ in range(grid[i][j] - 1):
-                        left.append([i, j])
-
-        return minimumCostMatch(left, right)
+                while grid[i][j] > 1:
+                    left.append([i, j])
+                    grid[i][j] -= 1
+        n = len(left)
+        cost = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                currentCost = 0
+                for k in range(2):
+                    currentCost += abs(left[i][k] - right[j][k])
+                cost[i][j] = currentCost
+        
+        @cache
+        def solve(leftBit: int, rightBit: int) -> int:
+            if leftBit == (1 << n) - 1:
+                return 0
+            res = float('inf')
+            for i in range(n):
+                if (leftBit >> i) ^ 1:
+                    for j in range(n):
+                        if (rightBit >> j) ^ 1 == 1:
+                            res = min(res, cost[i][j] + solve(leftBit | (1 << i), rightBit | (1 << j)))
+            return res
+        
+        return solve(0, 0)
