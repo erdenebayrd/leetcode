d414683b66eccc6f43e4ad5ffd319a6070bb2088
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-11 17:35:06 +1100

    Time: 43 ms (87.44%), Space: 19.8 MB (54.11%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -3,21 +3,11 @@ from functools import cache
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         n = len(s)
-        @cache
-        def suffixZeros(index: int) -> int:
-            if index >= n:
-                return 0
-            return int(s[index] == '0') + suffixZeros(index + 1)
-
-        @cache
-        def solve(index: int) -> int:
-            if index >= n:
-                return 0
-            if s[index] == '1':
-                result = suffixZeros(index)
-                result = min(result, 1 + solve(index + 1))
-            else:
-                result = solve(index + 1)
-            return result
-        
-        return solve(0)
+        countOnes = 0
+        flips = 0
+        for ch in s:
+            if ch == '1':
+                countOnes += 1
+            else: # ch == '0'
+                flips = min(flips + 1, countOnes)
+        return flips
