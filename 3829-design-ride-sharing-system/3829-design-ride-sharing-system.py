a30caa6df6d1a514262df381e9ee845334459972
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-12 13:16:39 +1100

    Time: 36 ms (91.36%), Space: 20.2 MB (56.2%) - LeetHub

--- 3829-design-ride-sharing-system/3829-design-ride-sharing-system.py
+++ 3829-design-ride-sharing-system/3829-design-ride-sharing-system.py
@@ -1,38 +1,35 @@
 from collections import deque
 
 class RideSharingSystem:
-
     def __init__(self):
-        self.riders = deque()
         self.drivers = deque()
-        self.currentRiders = set() # hash map for currentRiders
-        self.cancelledRiders = set() # lazy deletion from riders
+        self.riders = deque()
+        self.currentActiveRiders = set()
+        self.lazyDeletedRiders = set()
+        
 
     def addRider(self, riderId: int) -> None: # O(1)
         self.riders.append(riderId)
-        self.currentRiders.add(riderId)
+        self.currentActiveRiders.add(riderId)
 
     def addDriver(self, driverId: int) -> None: # O(1)
         self.drivers.append(driverId)
 
-    def matchDriverWithRider(self) -> List[int]:
-        while self.riders and self.riders[0] in self.cancelledRiders:
-            cancelledRider = self.riders.popleft()
-            self.cancelledRiders.discard(cancelledRider)
-        if self.riders and self.drivers:
-            rider = self.riders.popleft()
-            driver = self.drivers.popleft()
-            self.currentRiders.discard(rider)
-            return [driver, rider]
-        return [-1, -1]
+    def matchDriverWithRider(self) -> List[int]: # O(N) amortized, overall O(N) for all calls
+        while self.riders and self.riders[0] in self.lazyDeletedRiders:
+            riderId = self.riders.popleft()
+            self.lazyDeletedRiders.discard(riderId)
+        if not self.riders or not self.drivers:
+            return [-1, -1]
+        riderId = self.riders.popleft()
+        driverId = self.drivers.popleft()
+        return [driverId, riderId]
 
-
-    def cancelRider(self, riderId: int) -> None:
-        if riderId not in self.currentRiders:
+    def cancelRider(self, riderId: int) -> None: # O(1)
+        if riderId not in self.currentActiveRiders:
             return
-        self.cancelledRiders.add(riderId)
-        self.currentRiders.discard(riderId)
-
+        self.lazyDeletedRiders.add(riderId)
+        self.currentActiveRiders.discard(riderId)
 
 
 # Your RideSharingSystem object will be instantiated and called as such:
