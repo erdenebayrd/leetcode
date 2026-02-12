cc9ff997f7d425e56528a1b613521d980d60c917
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-12 15:14:01 +1100

    Time: 163 ms (72.99%), Space: 49.5 MB (36.46%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,43 @@
+import bisect
+
+class SnapshotArray:
+
+    def __init__(self, length: int): # O(N)
+        self.snapId = 0
+        self.array = [[[self.snapId, 0]] for _ in range(length)]
+
+    def set(self, index: int, val: int) -> None: # O(1)
+        snapId, value = self.array[index][-1]
+        if snapId == self.snapId:
+            value = val
+            self.array[index][-1] = [snapId, value]
+        else: # snapId != self.snapid (updated)
+            self.array[index].append([self.snapId, val])
+
+    def snap(self) -> int: # O(1)
+        self.snapId += 1
+        return self.snapId - 1
+
+    def get(self, index: int, snap_id: int) -> int: # O(Log N) N is the number of snap() called
+        # print(self.array)
+        snapshots = self.array[index] # contains [version, value], [version, value] etc
+        # print(snapshots)
+        # snapshotIndex = bisect.bisect_left(snapshots, [snap_id, -1])
+        low, high = -1, len(snapshots)
+        while low + 1 < high:
+            middle = (low + high) // 2
+            version = snapshots[middle][0]
+            if snap_id >= version:
+                low = middle
+            else: # snap_id < version
+                high = middle
+
+        snapshotIndex = low
+        return snapshots[snapshotIndex][-1]
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id) 
