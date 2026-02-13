37f8fec4b55e5a23003600f2cd1477bc08b0e832
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-14 01:01:54 +1100

    Time: 105 ms (46.54%), Space: 27.7 MB (76.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,17 @@
-import heapq
+from collections import deque
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # time: O(N * Log N)
+        # time: O(N)
         # space: O(N)
-        minHeapWithIndex = []
         n = len(temperatures)
         result = [0] * n
-        for index in range(n):
-            
-            while minHeapWithIndex and minHeapWithIndex[0][0] < temperatures[index]:
-                _, previousIndex = heapq.heappop(minHeapWithIndex)
-                result[previousIndex] = index - previousIndex
-            
-            heapq.heappush(minHeapWithIndex, [temperatures[index], index])
+        monotonicDecreasing = deque() # contains indexes of temperatures which is "day"
+        for index in range(n): # day
+            while monotonicDecreasing and temperatures[monotonicDecreasing[-1]] < temperatures[index]:
+                previousIndex = monotonicDecreasing.pop()
+                result[previousIndex] = index - previousIndex # how many days needed to be waited from day "previousIndex"
+            monotonicDecreasing.append(index)
         
+        del monotonicDecreasing
         return result
