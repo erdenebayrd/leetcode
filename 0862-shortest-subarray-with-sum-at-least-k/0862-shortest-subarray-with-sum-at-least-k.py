730d7252cfefc0d1d7c7b5a5c7f71636f8b95602
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-14 00:40:26 +1100

    Time: 205 ms (61.82%), Space: 33.8 MB (84.52%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,21 +1,27 @@
-import heapq
+from collections import deque
 
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         # time: O(N Log N)
         # space: O(N)
         n = len(nums)
-        minValueWithIndex = []
-        heapq.heappush(minValueWithIndex, [0, -1])
-        result = float('inf')
         cumulativeSum = 0
+        monotonicIncreasingWithIndex = deque()
+        result = float('inf')
         for i in range(n):
             cumulativeSum += nums[i]
-            while minValueWithIndex and cumulativeSum - minValueWithIndex[0][0] >= k:
-                _, index = heapq.heappop(minValueWithIndex)
+            if cumulativeSum >= k:
+                result = min(result, i + 1)
+            
+            while monotonicIncreasingWithIndex and cumulativeSum - monotonicIncreasingWithIndex[0][0] >= k:
+                _, index = monotonicIncreasingWithIndex.popleft()
                 result = min(result, i - index)
-            heapq.heappush(minValueWithIndex, [cumulativeSum, i])
 
+            while monotonicIncreasingWithIndex and monotonicIncreasingWithIndex[-1][0] >= cumulativeSum:
+                monotonicIncreasingWithIndex.pop()
+            
+            monotonicIncreasingWithIndex.append([cumulativeSum, i])
+        
         if result == float('inf'):
             result = -1
         return result
