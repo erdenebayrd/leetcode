20a668635463f76f15300acf7e19e5d614b9471a
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-14 12:40:38 +1100

    Time: 943 ms (5.01%), Space: 72.3 MB (13.73%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,46 +1,55 @@
-class UnionFindDisjointSet:
-    def __init__(self):
+from collections import defaultdict
+from sortedcontainers import SortedDict
+
+class UnionFind:
+    def __init__(self) -> None:
         self.parent = {}
     
-    def findParent(self, node: int):
+    def findParent(self, node: int) -> int:
         if node not in self.parent:
             self.parent[node] = node
         if self.parent[node] == node:
             return node
         self.parent[node] = self.findParent(self.parent[node])
         return self.parent[node]
-
-    def union(self, nodeX: int, nodeY: int) -> None:
-        parentX = self.findParent(nodeX)
-        parentY = self.findParent(nodeY)
-        if parentX == parentY:
-            return
-        self.parent[parentX] = self.parent[parentY]
+    
+    def isConnected(self, nodeU: int, nodeV: int) -> bool:
+        parentU = self.findParent(nodeU)
+        parentV = self.findParent(nodeV)
+        return parentU == parentV
+    
+    def connect(self, nodeU: int, nodeV: int) -> None:
+        parentU = self.findParent(nodeU)
+        parentV = self.findParent(nodeV)
+        if parentU != parentV:
+            self.parent[parentU] = parentV
 
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        secret = set([0, firstPerson])
-        meetings.sort(key=lambda m: m[2]) # sort by time
-        meets = {}
-        for x, y, time in meetings:
-            if time not in meets:
-                meets[time] = []
-            meets[time].append([x, y])
+        meetingsByTime = SortedDict()
+        for nodeU, nodeV, time in meetings:
+            if time not in meetingsByTime:
+                meetingsByTime[time] = []
+            meetingsByTime[time].append([nodeU, nodeV])
         
-        for time in meets:
-            localPool = UnionFindDisjointSet()
-            users = set()
-            for x, y in meets[time]:
-                localPool.union(x, y)
-                users.add(x)
-                users.add(y)
-            parents = {}
-            for user in users:
-                parent = localPool.findParent(user)
-                if parent not in parents:
-                    parents[parent] = set()
-                parents[parent].add(user)
+        pool = set([0, firstPerson]) # people who know the secret
+
+        for time in meetingsByTime:
+            unionFind = UnionFind()
+            edges = meetingsByTime[time]
+            for nodeU, nodeV in edges:
+                unionFind.connect(nodeU, nodeV) # union find used for detecting same component of each node or not
+
+            parents = defaultdict(set)
+            for nodeU, nodeV in edges:
+                parentU = unionFind.findParent(nodeU)
+                parentV = unionFind.findParent(nodeV)
+                parents[parentU].add(nodeU)
+                parents[parentV].add(nodeV)
+            
             for parent in parents:
-                if any([user in secret for user in parents[parent]]):
-                    secret.update(parents[parent])
-        return list(secret)
+                children = parents[parent]
+                if children & pool: # at least one common person
+                    pool |= children # extended by children
+        
+        return list(pool)
