b0305115c307abc14aa067dc59b57651b9503bbc
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-15 16:12:43 +1100

    Time: 41 ms (74.3%), Space: 34.3 MB (38.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -23,29 +23,46 @@ class Solution:
                 right -= 1
             return True
         
+        # result = []
+        # n = len(s) # aab n = 3 -> 2 ^ 2 = 4 total options to split 
+        # for bit in range(1 << (n - 1)): # 00 -> 01 -> 10 -> 11 O(2 ^ N)
+        #     indices = [] # contains [left, right] indexes of s. For example: aab, [[0, 0], [1, 2]] = [a, ab]
+        #     left, right = 0, n - 1          #                                  1   0
+        #     for indexOfBit in range(n - 1): # indexOfBit = 3 means 11 => a,  a,  b # 10 O(N)
+        #        # Left                                                        ^
+        #        # Right                                                   ^
+        #         if bit & (1 << indexOfBit): # we would use indexOfBit'th splitter
+        #             right = indexOfBit
+        #             indices.append([left, right])
+        #             left = right + 1
+        #     indices.append([left, n - 1])
+        #     # print(indices)
+        #     palindrome = True
+        #     for left, right in indices:
+        #         palindrome &= isPalindrome(left, right)
+        #     if palindrome is True:
+        #         # print(indices)
+        #         nestedResult = []
+        #         for left, right in indices:
+        #             nestedResult.append(s[left:right + 1])
+        #         result.append(nestedResult)
+        # # time: O(N * 2 ^ N)
+        # # space: O(N * 2 ^ N)
+        # return result
+
+        # --------------------------------------------- Recursive method ---------------------------------------------
         result = []
-        n = len(s) # aab n = 3 -> 2 ^ 2 = 4 total options to split 
-        for bit in range(1 << (n - 1)): # 00 -> 01 -> 10 -> 11 O(2 ^ N)
-            indices = [] # contains [left, right] indexes of s. For example: aab, [[0, 0], [1, 2]] = [a, ab]
-            left, right = 0, n - 1          #                                  1   0
-            for indexOfBit in range(n - 1): # indexOfBit = 3 means 11 => a,  a,  b # 10 O(N)
-               # Left                                                        ^
-               # Right                                                   ^
-                if bit & (1 << indexOfBit): # we would use indexOfBit'th splitter
-                    right = indexOfBit
-                    indices.append([left, right])
-                    left = right + 1
-            indices.append([left, n - 1])
-            # print(indices)
-            palindrome = True
-            for left, right in indices:
-                palindrome &= isPalindrome(left, right)
-            if palindrome is True:
-                # print(indices)
-                nestedResult = []
-                for left, right in indices:
-                    nestedResult.append(s[left:right + 1])
-                result.append(nestedResult)
-        # time: O(N * 2 ^ N)
-        # space: O(N * 2 ^ N)
+        currentResult = []
+        n = len(s)
+        def palindromeParition(startIndex: int) -> None:
+            if startIndex == n:
+                result.append(currentResult[:])
+                return
+            for rightIndex in range(startIndex, n):
+                if isPalindrome(startIndex, rightIndex) is True:
+                    currentResult.append(s[startIndex:rightIndex + 1])
+                    palindromeParition(rightIndex + 1)
+                    currentResult.pop()
+        
+        palindromeParition(0)
         return result
