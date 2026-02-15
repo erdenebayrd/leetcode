d2f42af51efd82fc03add7518723e7e6aed4615b
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-15 21:45:57 +1100

    Time: 3 ms (80.39%), Space: 20.7 MB (5.68%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -13,15 +13,33 @@ class Solution:
 
         # O(N * Log N)
         # O(1)
-        for index in range(len(numbers)):
-            need = target - numbers[index]
-            low, high = index, len(numbers)
-            while low + 1 < high:
-                middle = (low + high) // 2
-                if numbers[middle] < need:
-                    low = middle
-                elif numbers[middle] > need:
-                    high = middle
-                else: # numbers[middle] == need:
-                    return [index + 1, middle + 1]
+        # for index in range(len(numbers)):
+        #     need = target - numbers[index]
+        #     low, high = index, len(numbers)
+        #     while low + 1 < high:
+        #         middle = (low + high) // 2
+        #         if numbers[middle] < need:
+        #             low = middle
+        #         elif numbers[middle] > need:
+        #             high = middle
+        #         else: # numbers[middle] == need:
+        #             return [index + 1, middle + 1]
+        # return [-1, -1]
+
+        # O(N)
+        # O(1)
+        #            [1, 3, 4, 10] target = 7
+        # left.          ^
+        # right             ^
+        # sum = 5 
+        # sum < target
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total > target:
+                right -= 1
+            elif total < target:
+                left += 1
+            else: # total == target
+                return [left + 1, right + 1]
         return [-1, -1]
