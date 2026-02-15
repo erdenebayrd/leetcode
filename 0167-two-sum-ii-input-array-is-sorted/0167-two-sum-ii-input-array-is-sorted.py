9c87c7360ea70ea6326adc6e05b48bca77439884
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-15 21:41:09 +1100

    Time: 39 ms (5.06%), Space: 20.7 MB (27.3%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,27 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # time: O(N)
-        # space: O(N)
-        seen = {}
-        result = [] # indexes
-        for i in range(len(numbers)):
-            need = target - numbers[i]
-            if need in seen:
-                return [seen[need] + 1, i + 1]
-            seen[numbers[i]] = i
+        # # time: O(N)
+        # # space: O(N)
+        # seen = {}
+        # result = [] # indexes
+        # for i in range(len(numbers)):
+        #     need = target - numbers[i]
+        #     if need in seen:
+        #         return [seen[need] + 1, i + 1]
+        #     seen[numbers[i]] = i
+        # return [-1, -1]
+
+        # O(N * Log N)
+        # O(1)
+        for index in range(len(numbers)):
+            need = target - numbers[index]
+            low, high = index, len(numbers)
+            while low + 1 < high:
+                middle = (low + high) // 2
+                if numbers[middle] < need:
+                    low = middle
+                elif numbers[middle] > need:
+                    high = middle
+                else: # numbers[middle] == need:
+                    return [index + 1, middle + 1]
         return [-1, -1]
