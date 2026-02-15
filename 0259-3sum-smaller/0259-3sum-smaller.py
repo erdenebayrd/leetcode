072b67fd3eb2126d4ce8d3c6e971b302b94a40c8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-15 22:28:16 +1100

    Time: 552 ms (68%), Space: 19.5 MB (27.2%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,15 +1,33 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        res = 0
+        # time: O(N ^ 2)
+        # space: O(1)
+        # method: 3 pointers/sliding window
+
+        # step 1
+        # sort an array 
+
+        # step 2
+        # 3 pointers
+        #               [-2,0,1,3], target = 2
+        # left              ^
+        # middle              ^
+        # right                 ^
+        # currentSum = -2 + 1 + 3 = 2 < 2 False, counting left, middle, right whose sum are lower than target
+        # if currentSum < target, 
+        # result increased by right - middle = 3 - 1 = 2, result = 2
+        
+        nums.sort() # O(N*LogN)
         n = len(nums)
-        for le in range(n):
-            md = le + 1
-            ri = n - 1
-            while md < ri:
-                if nums[le] + nums[md] + nums[ri] < target:
-                    res += ri - md # (le, md, ri), (le, md, ri - 1), (le, md, ri - 2), ... , (le, md, md + 1) # all these triplets could be okay
-                    md += 1
-                else:
-                    ri -= 1
-        return res
+        result = 0
+        for left in range(n - 2):
+            middle = left + 1
+            right = n - 1
+            while middle < right:
+                if nums[left] + nums[middle] + nums[right] >= target:
+                    right -= 1
+                else: # nums[left] + nums[middle] + nums[right] < target:
+                    result += right - middle
+                    middle += 1
+
+        return result
