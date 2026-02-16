e58c8201a441b81cac3107f38a8db230918703dd
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-16 17:25:20 +1100

    Time: 0 ms (100%), Space: 19.2 MB (61.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,9 +4,22 @@ class Solution:
         # space: O(N)
         if n <= 1:
             return n
+        
         dp = [0] * n
         dp[0] = 1
         dp[1] = 2
         for stair in range(2, n):
             dp[stair] = dp[stair - 1] + dp[stair - 2]
         return dp[n - 1]
+
+        # 1, 2, 3, 5, 8, 13 .....  
+        # [ 1   1 ]     dp[1].     dp[2]
+        #           X         =.   
+        # [ 1   0 ].    dp[0]      dp[1]
+
+        # n - 1 element by power n - 2 times
+
+        # [[1, 1], [1, 0]] ^ (n - 2) * [2, 1] = dp[n - 1], dp[n - 2]
+        # time:  O(Log N)
+        # space: O(1) 
+        # using powLog
