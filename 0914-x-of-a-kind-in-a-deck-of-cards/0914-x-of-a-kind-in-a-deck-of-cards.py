93e2aa93a7be013253cfeb73fbff45f4808783da
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-17 22:10:53 +1100

    Time: 3 ms (77.71%), Space: 19.7 MB (67.47%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -8,6 +8,8 @@ class Solution:
         # 5: 9
         # x = 3, [111], [111], [555], [555], [555]
 
+        # tc: O(N + K * log M) N number of elements in deck, K is number of unique elements, M is maximum counted value
+        # sc: O(K)
         if len(deck) == 0:
             return False
         
