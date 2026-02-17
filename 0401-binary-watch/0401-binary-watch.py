e063d20466e94e57ba3a630893ff58991b9c82cf
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-17 22:00:32 +1100

    Time: 0 ms (100%), Space: 19.4 MB (60.29%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -9,40 +9,17 @@ class Solution:
             
             turnedOn = 1 
             Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
-
-            in total we have 10 Leds available. 
-            O(N * 2^ N) N = 10 max value of turned on is 10
-            O(2 ^ turnedOn) time has to be 19:01 ->> 7:01
-            bitmask solution
         """
 
-        def convertToTime(number: int) -> str:
-            minute = 0
-            hour = 0
-            for index in range(6):
-                if number & (1 << index):
-                    minute += (1 << index)
-            for index in range(4):
-                if number & (1 << (index + 6)):
-                    hour += (1 << index)
-            if 0 <= hour <= 11 and 0 <= minute <= 59:
-                result = str(hour) + ":"
-                if minute < 10:
-                    result = result + "0" + str(minute)
-                else:
-                    result = result + str(minute)
-                return result
-            return ""
-        
-        n = 10
         result = []
-        for bitmask in range(1 << n):
-            countOnes = 0
-            for index in range(n):
-                if (1 << index) & bitmask:
-                    countOnes += 1
-            if countOnes == turnedOn:
-                candidate = convertToTime(bitmask)
-                if candidate != "":
-                    result.append(candidate)
+        for hour in range(12):
+            for minute in range(60):
+                if hour.bit_count() + minute.bit_count() == turnedOn:
+                    currentTime = str(hour)
+                    if minute < 10:
+                        currentTime = currentTime + ":0" + str(minute)
+                    else:
+                        currentTime = currentTime + ":" + str(minute)
+                    result.append(currentTime)
         return result
+                    
