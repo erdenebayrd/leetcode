b1f3a99cd42a9088f9e665e475e739cbea4743ac
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-18 16:09:37 +1100

    Time: 55 ms (39.68%), Space: 31.2 MB (33.22%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,15 +1,38 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # tc: O(N)
-        # sc: O(N)
+        # # tc: O(N)
+        # # sc: O(N)
+        # n = len(nums)
+        # existed = [False] * (n + 1)
 
+        # for i in range(n):
+        #     if 1 <= nums[i] <= n:
+        #         existed[nums[i]] = True
+        # for i in range(1, n + 1):
+        #     if existed[i] is False:
+        #         return i
+        # return n + 1
+
+        # time: O(N)
+        # space: O(1)
         n = len(nums)
-        existed = [False] * (n + 1)
-
         for i in range(n):
             if 1 <= nums[i] <= n:
-                existed[nums[i]] = True
-        for i in range(1, n + 1):
-            if existed[i] is False:
-                return i
+                continue
+            nums[i] = float("inf")
+        
+        for i in range(n):
+            if 1 <= nums[i] <= n:
+                index = nums[i] - 1
+                if nums[index] > 0:
+                    nums[index] = -nums[index]
+            elif -n <= nums[i] <= -1: # negative number
+                index = -nums[i] - 1
+                if nums[index] > 0:
+                    nums[index] = -nums[index]
+
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
         return n + 1
