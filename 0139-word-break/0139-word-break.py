75062e57b3714ff0a7b0964bd8784c7b943a7399
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-02-18 21:42:54 +1100

    Time: 3 ms (70.55%), Space: 20.8 MB (5.1%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,27 +1,42 @@
 from functools import cache
 from collections import defaultdict
 
+class Trie:
+    def __init__(self):
+        self.isWord = False
+        self.children = {}
+    
+    def insert(self, node, word):
+        n = len(word)
+        for i in range(n):
+            ch = word[i]
+            if ch not in node.children:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isWord = True
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        words = defaultdict(bool)
+        # tc: O(N ^ 2) canBreak startIndex be [1, n], inside each recursive, for loop from 1 to n 
+        # space: O(M) M is total character of wordDict
+        n = len(s)
+        trie = Trie()
         for word in wordDict:
-            words[word] = True
-
-        # leet code
-        #      ^   
-        @cache
-        def canBreak(startIndex: int) -> bool:
-            if startIndex == len(s):
-                return True
-            result = False
-            currentSubWord = ""
-            for index in range(startIndex, len(s)):
-                currentSubWord += s[index]
-                if currentSubWord in words:
-                    result |= canBreak(index + 1)
-            return result
+            trie.insert(trie, word)
         
-        # time: O(N ^ 3)
-        # space: O(N)
+        @cache
+        def canBreak(startIndex):
+            if startIndex >= n:
+                return True
+            node = trie # start from root
+            for index in range(startIndex, n):
+                ch = s[index]    
+                if ch not in node.children:
+                    break
+                node = node.children[ch]
+                if node.isWord:
+                    if canBreak(index + 1):
+                        return True
+            return False
+        
         return canBreak(0)
