4bfd3962a6d0a38e1a3dbfbab0815f71987d9a5c
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-09 13:15:55 +1000

    Time: 6482 ms (22.46%), Space: 70.4 MB (53.19%) - LeetHub

--- 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.py
+++ 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.py
@@ -1,33 +1,48 @@
 class Solution:
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
-        # time: O(Sqrt(N) * Q)
+        # time: O(Sqrt(N) * (Q + N))
         # space: O(N)
-        # method: Sqrt decomposition
+        # method: Sqrt decomposition by Queries
+        """
+        check each queries
+            - if step size of query is greater than or equal to Sqrt(N), Just simply update nums by the query O(Sqrt(N))
+            - if step size of query is lower than Sqrt(N), group them by step size (at most Sqrt(N) groups will be created)
+                - after grouping them by step size
+                - iterate through each group and multiply [left] [diff] by "value" of the query, [right + step] divided by value (here we use Ferma's little theorom)
+                - time complexity of this one would be Sqrt(N) "which is number of groups" * N "nums would be updated by [diff]"
+        """
         n = len(nums)
+        groups = defaultdict(list)
+        sqrtN = int(n ** 0.5)
         mod = int(1e9 + 7)
-        sqrtSize = int(n ** 0.5)
-        group = defaultdict(list)
-        for left, right, step, value in queries: # O( Q * Sqrt(N) )
-            if step < sqrtSize:
-                group[step].append([left, right, value])
-            else:
-                for index in range(left, right + 1, step):
-                    nums[index] = (nums[index] * value) % mod
+        for left, right, step, value in queries: # O(Q)
+            if step >= sqrtN: # apply at a time
+                for i in range(left, right + 1, step): # O(Sqrt(N))
+                    nums[i] = (nums[i] * value) % mod
+            else: # step is too small to apply at a time
+                groups[step].append([left, right, value])
         
-        for step in range(1, sqrtSize): # O( Sqrt(N) * (Q + N) )
-            if not group[step]:
-                continue
-            diff = [1] * n
-            for left, right, value in group[step]:
-                diff[left] = (diff[left] * value) % mod
-                end = left + ((right - left) // step) * step + step
-                if end < n:
-                    diff[end] = (diff[end] * pow(value, mod - 2, mod)) % mod
-            for index in range(step, n):
-                diff[index] = (diff[index] * diff[index - step]) % mod
-            for index in range(n):
-                nums[index] = (nums[index] * diff[index]) % mod
+        # size of "keys" groups at most Sqrt(N)
+        for step in groups: # O(Sqrt(N))
+            diff = [1] * n # O(N)
+            for left, right, value in groups[step]: # O(Q)
+                diff[left] = diff[left] * value
+                endAt = left + ((right - left) // step) * step
+                # here we use Ferma's little theorom for dividing
+                # diff[endAt + step] = diff[endAt + step] / value
+                # Ferma's little theorom: a ^ (prime - 1) = 1 module by "prime"
+                # divided by "a" on both sides ( or we can say multiplied by a ^ -1 on both sides)
+                # a ^ (prime - 1) * a ^ -1 = a ^ -1
+                # a ^ (prime - 2) = a ^ -1
+                # "a ^ (prime - 2)" this would be used here for dividing
+                # diff[endAt + step] = diff[endAt + step] / value
+                if endAt + step < n:
+                    diff[endAt + step] = (diff[endAt + step] * pow(value, mod - 2, mod)) % mod
+            for i in range(step, n): # O(N)
+                diff[i] = diff[i] * diff[i - step] % mod
+            for i in range(n): # O(N) updating "nums" array
+                nums[i] = nums[i] * diff[i] % mod
         result = 0
-        for i in range(n):
-            result ^= nums[i]
+        for number in nums:
+            result ^= number
         return result
