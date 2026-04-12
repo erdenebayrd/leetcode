d49fec80da04d1371128b427d510a46e3b40733e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-12 20:15:37 +1000

    Time: 135 ms (40.71%), Space: 77.1 MB (54.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,48 +1,71 @@
+class Node:
+    def __init__(self, key=None, value=None, next=None, prev=None) -> None:
+        self.key = key
+        self.value = value
+        self.prev = prev
+        self.next = next
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.N = int(2e5 + 1)
-        self.ft = [0] * self.N
-        self.pos = {}
-        self.curPos = 1
-        self.capacity = capacity
-        self.val = {}
-
-    def add(self, pos: int, val: int) -> None:
-        while pos < self.N:
-            self.ft[pos] += val
-            pos += (-pos & pos)
-
-    def getSum(self, pos: int) -> int:
-        res = 0
-        while pos > 0:
-            res += self.ft[pos]
-            pos -= (-pos & pos)
-        return res
+        self.tail = self.head = Node()
     
-    def getRange(self, l: int, r: int) -> int:
-        return self.getSum(r) - self.getSum(l - 1)
+        self.size = 0
+        self.capacity = capacity
+        self.hashmap = {}        
+
+    def append(self, key: int, value: int) -> None:
+        node = Node(key=key, value=value)
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
+        self.hashmap[key] = self.tail
+        self.size += 1
+
+    def moveToEnd(self, key: int) -> None:
+        currentNode = self.hashmap[key]
+        
+        prevNode = currentNode.prev
+        nextNode = currentNode.next
+        if nextNode is None: # already at end
+            return
+        
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+
+        self.tail.next = currentNode
+        currentNode.prev = self.tail
+        currentNode.next = None
+
+        self.tail = currentNode
+
+    def popleft(self) -> None:
+        node = self.head.next
+        nextNode = node.next
+        self.head.next = nextNode
+        if nextNode is not None:
+            nextNode.prev = self.head
+        
+        node.prev = None
+        node.next = None
+        del self.hashmap[node.key]
+        self.size -= 1
 
     def get(self, key: int) -> int:
-        if key not in self.pos:
+        if key not in self.hashmap:
             return -1
-        x = self.getRange(self.pos[key], self.curPos)
-        if x <= self.capacity:
-            self.add(self.pos[key], -1)
-            self.pos[key] = self.curPos
-            self.add(self.curPos, 1)
-            self.curPos += 1
-            return self.val[key]
-        return -1
-        
-    def put(self, key: int, value: int) -> None:
-        self.val[key] = value
-        if key in self.pos:
-            self.add(self.pos[key], -1)
-        self.pos[key] = self.curPos
-        self.add(self.curPos, 1)
-        self.curPos += 1
+        self.moveToEnd(key)
+        return self.hashmap[key].value
 
+    def put(self, key: int, value: int) -> None:
+        if key not in self.hashmap:
+            self.append(key=key, value=value)
+        else:
+            self.hashmap[key].value = value
+            self.moveToEnd(key)
+        
+        while self.size > self.capacity:
+            self.popleft()
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
