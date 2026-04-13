31555f03997dbc95dccd58609dc20899a127f808
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-13 15:44:47 +1000

    Time: 937 ms (5.12%), Space: 19.4 MB (52.4%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canTransform(self, start: str, result: str) -> bool:
+        if Counter(start) != Counter(result):
+            return False
+        n = len(start)
+        start = list(start)
+        result = list(result)
+
+        def findNext(startIndex: int, ch: str, acceptable: str) -> int:
+            for i in range(startIndex + 1, n):
+                if ch == start[i]:
+                    return i
+                elif start[i] != acceptable:
+                    return -1
+            return -1
+
+        for i in range(n):
+            if start[i] == result[i]:
+                continue
+            elif start[i] == 'X' and result[i] == 'R':
+                return False
+            elif start[i] == 'X' and result[i] == 'L':
+                index = findNext(i, 'L', 'X')
+                if index == -1:
+                    return False
+                # swap
+                start[i], start[index] = start[index], start[i]
+            elif start[i] == 'L' and result[i] == 'X':
+                return False
+            elif start[i] == 'L' and result[i] == 'R':
+                return False
+            elif start[i] == 'R' and result[i] == 'X':
+                index = findNext(i, 'X', 'R')
+                if index == -1:
+                    return False
+                # swap
+                start[i], start[index] = start[index], start[i]
+            elif start[i] == 'R' and result[i] == 'L':
+                return False
+            else:
+                assert False
+        return True
