d1754a0bc16e13c365c07f05e5083b1ec5799980
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-13 21:57:37 +1000

    Time: 11 ms (91.62%), Space: 21.3 MB (10.47%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.py
+++ 1797-design-authentication-manager/1797-design-authentication-manager.py
@@ -1,28 +1,63 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key=None, value=None, prev=None, next=None):
+        self.key = key
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+        self.nodes = {} # by key
+    
+    def append(self, key, value) -> None:
+        node = Node(key=key, value=value)
+        endNode = self.tail.prev
+        endNode.next = node
+        node.prev = endNode
+        node.next = self.tail
+        self.tail.prev = node
+        self.nodes[key] = node
+        self.size += 1
+    
+    def remove(self, key) -> None:
+        if key not in self.nodes:
+            return
+        node = self.nodes[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.next = None
+        node.prev = None
+        del self.nodes[key]
+        self.size -= 1
 
 class AuthenticationManager:
 
     def __init__(self, timeToLive: int):
-        self.auths = OrderedDict()
+        self.auths = LinkedList()
         self.ttl = timeToLive
 
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self.auths[tokenId] = currentTime + self.ttl
+        self.auths.append(key=tokenId, value=currentTime + self.ttl)
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        if tokenId not in self.auths:
+        while self.auths.head.next.key is not None and self.auths.head.next.value <= currentTime:
+            self.auths.remove(self.auths.head.next.key)
+        if tokenId not in self.auths.nodes:
             return
-        if self.auths[tokenId] <= currentTime:
-            del self.auths[tokenId]
-            return
-        self.auths[tokenId] = currentTime + self.ttl
-        self.auths.move_to_end(tokenId, last=True)
+        node = self.auths.nodes[tokenId]
+        self.auths.remove(node.key)
+        node.value = currentTime + self.ttl
+        self.auths.append(node.key, node.value)
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        while len(self.auths) > 0 and self.auths[next(iter(self.auths))] <= currentTime:
-            del self.auths[next(iter(self.auths))]
-        # print(self.auths)
-        return len(self.auths)
+        while self.auths.head.next.key is not None and self.auths.head.next.value <= currentTime:
+            self.auths.remove(self.auths.head.next.key)
+        return self.auths.size
 
 # Your AuthenticationManager object will be instantiated and called as such:
 # obj = AuthenticationManager(timeToLive)
