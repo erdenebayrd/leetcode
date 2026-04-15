4f0ea595cc1ce13bec4c41c0d994fa8820e95d31
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-15 23:47:57 +1000

    Time: 7587 ms (5.49%), Space: 350.7 MB (6.59%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkPartitioning(self, s: str) -> bool:
+        # time: O(N ^ 3)
+        # space: O(N)
+        # method: DP
+
+        n = len(s)
+        @cache
+        def isPalindrome(left: int, right: int) -> bool:
+            if left >= right:
+                return True
+            result = s[left] == s[right]
+            result &= isPalindrome(left + 1, right - 1)
+            return result
+
+        @cache
+        def solve(index: int, k: int) -> bool:
+            if k == 0:
+                return index == n
+            result = False
+            for right in range(index, n):
+                if isPalindrome(index, right):
+                    result |= solve(right + 1, k - 1)
+            return result
+        
+        result = solve(0, 3)
+        solve.cache_clear()
+        isPalindrome.cache_clear()
+        return result
