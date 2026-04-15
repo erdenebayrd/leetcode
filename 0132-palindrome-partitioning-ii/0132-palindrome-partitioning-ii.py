2e0ad506b4d1e1332b83f40fde6d045d717bd089
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-15 23:57:17 +1000

    Time: 2804 ms (12.56%), Space: 758.4 MB (5.02%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        # time: O(N ^ 2)
+        # space: O(N)
+        # method: DP
+        @cache
+        def isPalindrome(left: int, right: int) -> bool:
+            if left >= right:
+                return True
+            result = s[left] == s[right]
+            result &= isPalindrome(left + 1, right - 1)
+            return result
+        
+        n = len(s)
+
+        @cache
+        def solve(index: int) -> int:
+            if index >= n:
+                return 0
+            result = 1 + solve(index + 1)
+            for right in range(index, n):
+                if isPalindrome(index, right):
+                    result = min(result, 1 + solve(right + 1))
+            return result
+        result = solve(0) - 1
+        return result
