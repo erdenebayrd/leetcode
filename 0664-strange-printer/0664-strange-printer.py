e3169c7f09d5e0b7c593df31bd376a9311d9678a
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-19 19:12:35 +1000

    Time: 227 ms (37.24%), Space: 36.9 MB (8.2%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -3,16 +3,14 @@ class Solution:
         # time: O(N ^ 3)
         # space: O(N ^ 2)
         # method: DP
-
         n = len(s)
-
         @cache
         def solve(left: int, right: int) -> int:
             if left > right or left >= n:
                 return 0
             result = 1 + solve(left + 1, right)
             for i in range(left + 1, right + 1):
-                if s[left] == s[i]:
+                if s[i] == s[left]:
                     result = min(result, solve(left + 1, i - 1) + solve(i, right))
             return result
         
