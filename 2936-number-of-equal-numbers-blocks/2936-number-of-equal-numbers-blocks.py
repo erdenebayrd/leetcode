bfbf47894d8fef511fc42515a0292b952aa2986c
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-22 21:34:42 +1000

    Time: 10281 ms (33.33%), Space: 52.1 MB (90.48%) - LeetHub

--- 2936-number-of-equal-numbers-blocks/2936-number-of-equal-numbers-blocks.py
+++ 2936-number-of-equal-numbers-blocks/2936-number-of-equal-numbers-blocks.py
@@ -12,24 +12,48 @@ class Solution(object):
 
         total distinct values are N
         """
-        # time: O(N * log(len(nums)))
-        # space: O(1)
-        # method: binary search
+        # # time: O(N * log(len(nums)))
+        # # space: O(1)
+        # # method: binary search
 
-        length = nums.size()
-        if length <= 1:
-            return length
+        # length = nums.size()
+        # if length <= 1:
+        #     return length
+        # result = 0
+        # index = 0
+        # while index < length:
+        #     value = nums.at(index)
+        #     low, high = index - 1, length
+        #     while low + 1 < high:
+        #         mid = (low + high) // 2
+        #         if nums.at(mid) != value:
+        #             high = mid
+        #         else:
+        #             low = mid
+        #     result += 1
+        #     index = low + 1
+        # return result
+
+        # time: O(N * Log (len(nums)) ^ 2)
+        # space: O(log(nums))
+        # method: binary indexed search
+        
+        def findNext(index: int, value: int) -> int:
+            length = nums.size()
+            if index >= length or nums.at(index) != value:
+                return index
+            distance = 1
+            current = index
+            while index + distance < length and nums.at(index + distance) == value:
+                current = index + distance
+                distance <<= 1
+            return findNext(current + 1, value)
+
         result = 0
         index = 0
+        length = nums.size()
         while index < length:
             value = nums.at(index)
-            low, high = index - 1, length
-            while low + 1 < high:
-                mid = (low + high) // 2
-                if nums.at(mid) != value:
-                    high = mid
-                else:
-                    low = mid
+            index = findNext(index, value)
             result += 1
-            index = low + 1
         return result
