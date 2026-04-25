e87f358eb040599d1a6afe3f5bbf90f909ee7918
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-25 13:26:27 +1000

    Time: 0 ms (100%), Space: 19.3 MB (56.28%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,26 +1,34 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        # time: O(log2(N) + log3(N) + ... log(N-2)(N))
-        # space: O(log2(N) + log3(N) + ... log(N-2)(N))
-        # method: simulation
+        # # time: O(log2(N) + log3(N) + ... log(N-2)(N))
+        # # space: O(log2(N) + log3(N) + ... log(N-2)(N))
+        # # method: simulation
 
-        def convertToBase(number: int, base: int) -> str:
-            result = ""
-            while number:
-                digit = number % base
-                result += str(digit)
-                number //= base
-            result = result[::-1]
-            return result
+        # def convertToBase(number: int, base: int) -> str:
+        #     result = ""
+        #     while number:
+        #         digit = number % base
+        #         result += str(digit)
+        #         number //= base
+        #     result = result[::-1]
+        #     return result
         
-        def isPalindrome(text: str) -> bool:
-            n = len(text)
-            for i in range(n//2):
-                if text[i] != text[n - 1 - i]:
-                    return False
-            return True
+        # def isPalindrome(text: str) -> bool:
+        #     n = len(text)
+        #     for i in range(n//2):
+        #         if text[i] != text[n - 1 - i]:
+        #             return False
+        #     return True
         
-        for base in range(2, n - 1):
-            if isPalindrome(convertToBase(n, base)) is False:
-                return False
-        return True
+        # for base in range(2, n - 1):
+        #     baseNumber = convertToBase(n, base)
+        #     if isPalindrome(baseNumber) is False:
+        #         return False
+        # return True
+
+        """
+            since any number greater than 4's base n - 2 number is alwas 12 which is not palindrome
+            and 4's base 2 is 10 which is not palindromic
+            basically there is no strictly palindromic number
+        """
+        return False
