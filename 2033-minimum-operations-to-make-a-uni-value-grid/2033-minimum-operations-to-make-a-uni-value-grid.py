000d85389953a356f3c13acf11378bfdbdedd0ed
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-04-28 18:08:14 +1000

    Time: 205 ms (28.08%), Space: 40.9 MB (56.85%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        # time: O(N * Log N)
+        # space: O(N)
+        arr = []
+        for row in grid:
+            for col in row:
+                arr.append(col)
+        if len(arr) == 1:
+            return 0
+        arr.sort()
+        n = len(arr)
+        for i in range(1, n):
+            if (arr[i] - arr[0]) % x != 0:
+                return -1
+        
+        res = 0
+        for el in arr:
+            res += abs(el - arr[n // 2]) // x
+        return res
