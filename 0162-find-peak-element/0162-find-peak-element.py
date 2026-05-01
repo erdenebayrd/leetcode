ababaf91cb535180e787e8047d2dbc35fc2af6ee
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-01 19:10:01 +1000

    Time: 0 ms (100%), Space: 19.2 MB (79.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,14 +1,31 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        """
+        since the problem condition says "nums[i] != nums[i + 1] for all valid i."
+        meaning there must be at least one PEAK element existed
+        since we can return ANY peak element's index, so that we can do binary search through the given array
+        if consecutive 3 elements of an array (including outside of an array which is -inf) is strictly increasing, we need to go right side
+        if consecutive 3 elements of an array (including outside of an array which is -inf) is strictly decreasing, we need to go left side
+        if we found an peak element, we can just return it's index, so that time complexity would be O( log N ), space: O(1)
+        """
+        def isIncreasing(index: int) -> bool:
+            prev = float('-inf') if index - 1 < 0 else nums[index - 1]
+            next = float('-inf') if index + 1 >= n else nums[index + 1]
+            return prev < nums[index] < next
+        
+        def isPeak(index: int) -> bool:
+            prev = float('-inf') if index - 1 < 0 else nums[index - 1]
+            next = float('-inf') if index + 1 >= n else nums[index + 1]
+            return prev < nums[index] > next
+
         n = len(nums)
-        if n == 1:
-            return 0
-        lo, hi = -1, n
-        nums.append(int(-1e18))
-        while lo + 1 < hi:
-            md = (lo + hi) // 2
-            if nums[md] > nums[md + 1]:
-                hi = md
+        low, high = -1, n
+        while low + 1 < high:
+            mid = (low + high) // 2
+            if isPeak(mid):
+                return mid
+            elif isIncreasing(mid):
+                low = mid
             else:
-                lo = md
-        return hi
+                high = mid
+        return -1
