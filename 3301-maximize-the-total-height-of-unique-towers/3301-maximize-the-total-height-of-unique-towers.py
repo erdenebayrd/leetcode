4f02e84d3c949b5a6ce8eea1bacdcb997cf3ea79
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-02 15:18:35 +1000

    Time: 184 ms (86.46%), Space: 34.2 MB (21.88%) - LeetHub

--- 3301-maximize-the-total-height-of-unique-towers/3301-maximize-the-total-height-of-unique-towers.py
+++ 3301-maximize-the-total-height-of-unique-towers/3301-maximize-the-total-height-of-unique-towers.py
@@ -4,19 +4,18 @@ class Solution:
     def maximumTotalSum(self, maximumHeight: List[int]) -> int:
         # time: O(N log N)
         # space: O(N)
-        # method: max heap
+        # method: sorting
 
+        maximumHeight.sort() # this requires O(N) spaces in builtin sort function
+        n = len(maximumHeight)
         lastTaken = float('inf')
-        arr = [-height for height in maximumHeight]
-        heapq.heapify(arr)
         result = 0
-        while arr:
-            height = -arr[0]
+        for i in range(n - 1, -1, -1):
+            height = maximumHeight[i]
             if lastTaken <= height:
                 height = lastTaken - 1
-                if height <= 0:
-                    return -1
+            if height <= 0:
+                return -1
             result += height
             lastTaken = height
-            heapq.heappop(arr)
         return result
