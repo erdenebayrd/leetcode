6b54a813382cabdc1086821f6ce1e00c0e65d7c1
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-02 23:35:48 +1000

    Time: 621 ms (72.64%), Space: 34.5 MB (31.86%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.py
@@ -0,0 +1,41 @@
+import bisect
+from collections import defaultdict
+
+class Solution:
+    def minOperations(self, nums: list[int]) -> int:
+        # time: O(N log N) N is the maximum prime number
+        # space: O(N)
+        # method sieve
+        
+        def sieve(N: int) -> int: # O(N)
+            isPrime = [True] * (N + 1)
+            isPrime[0] = isPrime[1] = False
+            primes = [2]
+            for i in range(3, N + 1, 2):
+                if isPrime[i] is False:
+                    continue
+                primes.append(i)
+                for j in range(i * i, N + 1, i):
+                    isPrime[j] = False
+            return primes
+        
+        primes = sieve(max(nums) * 2)
+        isPrime = set(primes)
+        
+        def nextPrime(number: int) -> int:
+            index = bisect.bisect_left(primes, number)
+            return primes[index]
+        
+        result = 0
+        n = len(nums)
+        for i in range(n):
+            if i & 1: # odd index (non-prime)
+                if nums[i] in isPrime:
+                    result += 1
+                    if nums[i] == 2:
+                        result += 1
+            else: # even index (prime)
+                if nums[i] not in isPrime:
+                    prime = nextPrime(nums[i])
+                    result += prime - nums[i]
+        return result
