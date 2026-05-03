a23373a5419a7c035d8b8e9e6eb56b0abf47de7f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-03 12:54:45 +1000

    Time: 0 ms (100%), Space: 19.4 MB (15.24%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,11 +1,29 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        # time: O(N * M)
+        # time: O(N + M)
         # space: O(N + M)
-        # method: string pattern search
-        n = len(s)
-        m = len(goal)
-        if n != m:
+        # method: KMP pattern search algo
+        if len(s) != len(goal):
             return False
-        goal = goal + goal
-        return s in goal
+        text = goal + goal
+        pattern = s
+        n = len(pattern)
+        lps = [0] * n
+        length = 0
+
+        for i in range(1, n):
+            while length > 0 and pattern[length] != pattern[i]:
+                length = lps[length - 1]
+            if pattern[length] == pattern[i]:
+                length += 1
+            lps[i] = length
+        
+        length = 0
+        for i in range(len(text)):
+            while length > 0 and pattern[length] != text[i]:
+                length = lps[length - 1]
+            if pattern[length] == text[i]:
+                length += 1
+            if length == len(pattern):
+                return True
+        return False
