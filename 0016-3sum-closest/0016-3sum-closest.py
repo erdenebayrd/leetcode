7508ce77d27fd7a3b6a3b2026111069cd5a8d14f
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-06 21:55:06 +1000

    Time: 425 ms (15.84%), Space: 19.2 MB (96.82%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,32 +1,23 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        # time: O(N ^ 3)
-        # space: O(1)
-        # method: brute-force
-        # left, middle, right indices
-
-        # 3 pointers/sliding window
         # time: O(N ^ 2)
-        # space: O(1)
-        # method: sliding window
-
-        def tripleSum(left: int, middle: int, right: int) -> int:
-            return nums[left] + nums[middle] + nums[right]
-
+        # space: O(1) but sorting use O(N)
+        # method: 2 pointers + sorting
         n = len(nums)
-        result = float('inf')
-        delta = float('inf')
-        nums.sort() # O(N * log N)
-        for left in range(n - 2): # O(N ^ 2)
-            middle = left + 1
-            right = n - 1
-            while middle < right:
-                threeSum = tripleSum(left, middle, right)
-                if abs(threeSum - target) < delta:
-                    delta = abs(threeSum - target)
-                    result = threeSum
-                if threeSum > target:
+        result = 0
+        closestDiff = float('inf')
+        nums.sort()
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                currentSum = nums[i] + nums[left] + nums[right]
+                currentDiff = abs(currentSum - target)
+                if currentDiff < closestDiff:
+                    closestDiff = currentDiff
+                    result = currentSum
+                
+                if currentSum > target:
                     right -= 1
-                else: # threeSum <= target:
-                    middle += 1
+                else: # currentSum <= target
+                    left += 1
         return result
