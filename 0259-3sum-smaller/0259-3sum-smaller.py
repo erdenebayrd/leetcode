d2dab88a09514e947816f5e71d9c8470186b2f0e
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-06 22:04:01 +1000

    Time: 580 ms (52.19%), Space: 19.2 MB (79.93%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,33 +1,20 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         # time: O(N ^ 2)
-        # space: O(1)
-        # method: 3 pointers/sliding window
+        # space: O(1) but sort() function uses O(N) space
+        # method: 2 pointers + sorting
 
-        # step 1
-        # sort an array 
-
-        # step 2
-        # 3 pointers
-        #               [-2,0,1,3], target = 2
-        # left              ^
-        # middle              ^
-        # right                 ^
-        # currentSum = -2 + 1 + 3 = 2 < 2 False, counting left, middle, right whose sum are lower than target
-        # if currentSum < target, 
-        # result increased by right - middle = 3 - 1 = 2, result = 2
-        
-        nums.sort() # O(N*LogN)
         n = len(nums)
+        nums.sort()
         result = 0
-        for left in range(n - 2):
-            middle = left + 1
-            right = n - 1
-            while middle < right:
-                if nums[left] + nums[middle] + nums[right] >= target:
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                currentSum = nums[i] + nums[left] + nums[right]
+                if currentSum >= target:
                     right -= 1
-                else: # nums[left] + nums[middle] + nums[right] < target:
-                    result += right - middle
-                    middle += 1
+                else: # currentSum < target
+                    result += right - left
+                    left += 1
 
         return result
