27eaa74b1f2728bdcdb598ccf89135730507cda4
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-06 21:07:55 +1000

    Time: 195 ms (19.07%), Space: 57 MB (33.27%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -4,7 +4,7 @@ class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         # time: O(rows * cols)
         # space: O(rows * cols)
-        # method: simulation + prefix sum
+        # method: simulation
 
         # rotating clockwise 90 degree
         def rotate(row: int, col: int) -> Tuple[int, int]:
@@ -15,26 +15,25 @@ class Solution:
 
         # initializing result
         result = [[""] * cols for _ in range(rows)]
-        gravity = [[1] * cols for _ in range(rows)]
 
         # rotating and copying
         for i in range(len(boxGrid)):
             for j in range(len(boxGrid[i])):
                 row, col = rotate(i, j)
                 result[row][col] = boxGrid[i][j]
-                if boxGrid[i][j] == '*' or boxGrid[i][j] == '#':
-                    gravity[row][col] = 0
-
-        for row in range(rows - 2, -1, -1):
-            for col in range(cols):
-                if gravity[row + 1][col] > 0 and result[row][col] != "*":
-                    gravity[row][col] += gravity[row + 1][col]
-
+        
         # apply gravity
-        for row in range(rows - 2, -1, -1):
-            for col in range(cols):
-                if result[row][col] == "#" and gravity[row][col] > 0:
-                    result[row][col] = '.'
-                    result[row + gravity[row][col]][col] = '#'
-
+        for col in range(cols):
+            lastRow = rows - 1
+            for row in range(rows - 1, -1, -1):
+                if result[row][col] == "#":
+                    if row == lastRow:
+                        lastRow -= 1
+                        continue
+                    result[row][col] = "."
+                    result[lastRow][col] = "#"
+                    lastRow -= 1
+                if result[row][col] == "*":
+                    lastRow = row - 1
+        
         return result
