5dffd8543247edf278ba077ea2b3c648803e522b
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-14 17:43:30 +1000

    Time: 0 ms (100%), Space: 19.1 MB (97.4%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,29 +1,38 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        # time: O(N + M)
-        # space: O(N + M)
-        # method: KMP pattern search algo
+        """
+        s = "abcde", goal = "cdeab"
+        abcdeabcde
+          cdeab
+        """
         if len(s) != len(goal):
             return False
-        text = goal + goal
-        pattern = s
-        n = len(pattern)
-        lps = [0] * n
-        length = 0
 
-        for i in range(1, n):
-            while length > 0 and pattern[length] != pattern[i]:
-                length = lps[length - 1]
-            if pattern[length] == pattern[i]:
-                length += 1
-            lps[i] = length
-        
+        text = goal + "#" + s + s
+        n = len(text)
+        left, right = -1, -1
+        z = [0] * n
         length = 0
-        for i in range(len(text)):
-            while length > 0 and pattern[length] != text[i]:
-                length = lps[length - 1]
-            if pattern[length] == text[i]:
+        for i in range(1, n):
+
+            if i <= right:
+                prev_i = i - left
+                prev_length = z[prev_i]
+                length = min(prev_length, right - i + 1)
+
+            while i + length < n and text[length] == text[i + length]:
                 length += 1
-            if length == len(pattern):
-                return True
-        return False
+
+            z[i] = length
+            if i + length - 1 >= right:
+                right = i + length - 1
+                left = i
+        # # """
+        # # ['a', 'b', 'c', 'd', '#', 'b', 'c', 'a', 'd', 'b', 'c', 'a', 'd']
+        # # [ 0,   0,   0,   0,   0,   0,   0,   1,   3,   4,   4,   4,   4 ]
+        # # """
+        # print(list(text))
+        # print(z)
+
+        return max(z) >= len(goal)
+        
