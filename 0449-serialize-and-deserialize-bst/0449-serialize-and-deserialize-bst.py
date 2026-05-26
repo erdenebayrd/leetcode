95a9e7fc02129e1ef827af42494926133107570b
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-26 13:17:47 +1000

    Time: 70 ms (34.36%), Space: 22 MB (97.12%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -10,39 +10,31 @@ class Codec:
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
         """
-        stack = [root]
-        encoder = []
-        while stack:
-            node = stack.pop()
-            if node:
-                encoder.append(str(node.val))
-                stack.append(node.right)
-                stack.append(node.left)
-            else:
-                encoder.append("#") # means null
-        encoded_string = ",".join(encoder)
-        return encoded_string
+        def post_order(node: Optional[TreeNode]) -> List[str]:
+            if not node:
+                return []
+            return post_order(node.left) + post_order(node.right) + [str(node.val)]
         
+        encoded = ",".join(post_order(root))
+        return encoded
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree.
         """
-        encoder = data.split(",")
-        if encoder[0] == "#":
+        if not data:
             return None
-        root = TreeNode(int(encoder[0]))
-        stack = [(root, "R"), (root, "L")]
-        for value in encoder[1:]:
-            parent, side = stack.pop()
-            if value == "#": # Null
-                continue
+        encoded = data.split(",")
+
+        def helper(low_limit: float, high_limit: float) -> Optional[TreeNode]:
+            if not encoded or not (low_limit <= int(encoded[-1]) <= high_limit):
+                return None
+            value = encoded.pop()
             node = TreeNode(int(value))
-            if side == "L": # left child of parent
-                parent.left = node
-            else: # right child of parent
-                parent.right = node
-            stack.append((node, "R"))
-            stack.append((node, "L"))
+            node.right = helper(node.val, high_limit)
+            node.left = helper(low_limit, node.val)
+            return node
+
+        root = helper(float("-inf"), float("inf"))
         return root
 
 # Your Codec object will be instantiated and called as such:
