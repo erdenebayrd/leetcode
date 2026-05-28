0e6fee5fde35600ee847e90115607d19e54e2044
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-05-28 14:13:16 +1000

    Time: 3 ms (75.15%), Space: 20.5 MB (55.77%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,41 +1,49 @@
-from collections import deque, defaultdict
+from collections import defaultdict, deque
 
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # time: O(V + E) V is number of nodes (numCourses), E is number edges len(prerequisites)
+    def findOrder(self, num_courses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+            graph problem
+            if there is a cycle, not possible to finish all courses
+            we can determine graph cycle using Kahn's algorithm
+            which works by 
+                "*" building adjacent list 
+                "*" counting graph input degrees
+                "*" start from nodes with 0 input degree
+                "*" visiting neighbors then decreasing degree of neighbors by 1 if the neighbors input degree become 0, add it to the queue
+                "*" after all check input degrees of all nodes if there is a node with input degree more than 0, there must be cycle
+
+            while detecting cycle by Kahn's algo we can do following to trace path
+                "*" order of nodes in the queue would be stored as paths in other variable
+                "*" if there is no cycle, we can just return the variable paths
+            
+            time complexity would be O(V + E) V is total number of nodes/vertices, E is total number of edges between nodes
+            space complexity would be O(V + E)
+        """
+        # time: O(V + E)
         # space: O(V + E)
-        # method: topological order + Kahn's algorithm
-
-        # numCourses = 6
-        # --------------
-        # 1 <- 0
-        # |    |
-        # V    V
-        # 3 <- 2
-
-        # 4 -> 5
-
-        # can be: 0 -> 4 -> 5 -> 1 -> 2 -> 3 
-        indegree = defaultdict(int)
-        graph = defaultdict(list)
-        for nodeU, nodeV in prerequisites:
-            graph[nodeV].append(nodeU)
-            indegree[nodeU] += 1
+        # method: graph Kahn's algorithm
+        adjacent_list = defaultdict(list)
+        in_degree = defaultdict(int)
+        for u, v in prerequisites:
+            adjacent_list[v].append(u)
+            in_degree[u] += 1
         
         queue = deque()
-        for node in range(numCourses):
-            if indegree[node] == 0:
+        for node in range(num_courses):
+            if in_degree[node] == 0:
                 queue.append(node)
         
-        topological = []
+        paths = []
         while queue:
             node = queue.popleft()
-            topological.append(node)
-            for neighbor in graph[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
+            paths.append(node)
+            for neighbor in adjacent_list[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
                     queue.append(neighbor)
         
-        if len(topological) != numCourses:
-            return []
-        return topological
+        for node in range(num_courses):
+            if in_degree[node] > 0:
+                return []
+        return paths
