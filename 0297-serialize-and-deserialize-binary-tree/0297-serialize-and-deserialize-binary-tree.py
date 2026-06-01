3aa22470d725f62d7d9fd65c1dd7650024b97247
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-06-01 13:40:21 +1000

    Time: 92 ms (42.72%), Space: 22.2 MB (98.79%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        encoded = []
+        def dfs(node: Optional[TreeNode], ref: list) -> None:
+            if not node:
+                ref.append("#") # null
+                return
+            ref.append(str(node.val))
+            dfs(node.left, ref)
+            dfs(node.right, ref)
+        dfs(root, encoded)
+        return ",".join(encoded)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        encoded = data.split(",")
+        if len(encoded) <= 1 or encoded[0] == "#": # Null
+            return
+        root = TreeNode(int(encoded[0]))
+        stack = [(root, "R"), (root, "L")]
+        for value in encoded[1:]:
+            parent, side = stack.pop()
+            if value == "#": # Null
+                continue
+            node = TreeNode(int(value))
+            if side == "L":
+                parent.left = node
+            else:
+                parent.right = node
+            stack.append((node, "R"))
+            stack.append((node, "L"))
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
