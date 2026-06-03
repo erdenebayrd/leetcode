dc4c3aa6d06385354c8c66108a1e3bb0c3efdced
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-06-03 22:39:28 +1000

    Time: 6 ms (38.53%), Space: 19.4 MB (27.06%) - LeetHub

--- 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.py
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.py
@@ -1,45 +1,48 @@
-from collections import defaultdict
+class SWAG:
+    def __init__(self) -> None:
+        self.in_stack = [] # contains tuple (first value is the number added, second value is current OR of all values in in_stack)
+        self.out_stack = []
+
+    def append(self, number: int) -> None:
+        in_aggregation = number
+        if self.in_stack:
+            in_aggregation |= self.in_stack[-1][-1]
+        self.in_stack.append((number, in_aggregation))
+    
+    def pop_left(self) -> None:
+        if not self.out_stack: # if out_stack is empty
+            out_aggregation = 0
+            while self.in_stack:
+                number, _ = self.in_stack.pop()
+                out_aggregation |= number
+                self.out_stack.append((number, out_aggregation))
+        
+        self.out_stack.pop()
+    
+    def query(self) -> int: # in this case, returns logical OR of the sliding window O(1)
+        in_aggregation = 0
+        if self.in_stack:
+            in_aggregation |= self.in_stack[-1][-1]
+        out_aggregation = 0
+        if self.out_stack:
+            out_aggregation |= self.out_stack[-1][-1]
+        return in_aggregation | out_aggregation
 
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        # time: O(N log max(nums))
-        # space: O(log(max(nums)))
-        # method: sliding window + count
+        # time: O(N)
+        # space: O(N)
+        # method: Sliding Window Aggregation
         n = len(nums)
         left = 0
-        count = defaultdict(int)
-
-        def add(number: int, ref: defaultdict) -> None:
-            significant_bit = number.bit_length() - 1
-            for i in range(significant_bit, -1, -1):
-                if (1 << i) & number:
-                    ref[i] += 1    
-        
-        def remove(number: int, ref: defaultdict) -> None:
-            significant_bit = number.bit_length() - 1
-            for i in range(significant_bit, -1, -1):
-                if (1 << i) & number:
-                    ref[i] -= 1
-                    if ref[i] == 0:
-                        del ref[i]
-
-        def is_greater(ref: defaultdict, k: int) -> bool:
-            number = 0
-            for bit in ref:
-                number |= (1 << bit)
-            return number >= k
-        
-        result = float("inf")
-        
+        swag = SWAG()
+        result = float('inf')
         for right in range(n):
-            number = nums[right]
-            add(number, count)
-            while left <= right and is_greater(count, k):
+            swag.append(nums[right])
+            while left <= right and swag.query() >= k:
                 result = min(result, right - left + 1)
-                number = nums[left]
-                remove(number, count)
+                swag.pop_left()
                 left += 1
         if result == float("inf"):
             result = -1
         return result
-            
