68540cac40a74391ed3e34b7cffac90b5f286687
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-06-03 18:03:40 +1000

    Time: 962 ms (5.1%), Space: 37.5 MB (11.22%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,27 +1,35 @@
-from collections import deque
-
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        # time: O(N)
-        # space: O(N)
+        # time: O(N log N)
+        # space O(N)
+        # method: monotonic strictly increasing stack + binary search
         n = len(nums)
-        cumulativeSum = 0
-        monotonicIncreasingWithIndex = deque()
-        result = float('inf')
-        for i in range(n):
-            cumulativeSum += nums[i]
-            if cumulativeSum >= k:
-                result = min(result, i + 1)
-            
-            while monotonicIncreasingWithIndex and cumulativeSum - monotonicIncreasingWithIndex[0][0] >= k:
-                _, index = monotonicIncreasingWithIndex.popleft()
-                result = min(result, i - index)
-
-            while monotonicIncreasingWithIndex and monotonicIncreasingWithIndex[-1][0] >= cumulativeSum:
-                monotonicIncreasingWithIndex.pop()
-            
-            monotonicIncreasingWithIndex.append([cumulativeSum, i])
+        prefix = [0] * n
+        prefix[0] = nums[0]
+        if prefix[0] >= k:
+            return 1
         
-        if result == float('inf'):
+        result = float('inf')
+        for i in range(1, n):
+            prefix[i] += prefix[i - 1] + nums[i]
+            if prefix[i] >= k:
+                result = min(result, i + 1)
+        
+        stack = [] # contains index only
+        for i in range(n):
+            while stack and prefix[stack[-1]] >= prefix[i]:
+                stack.pop()
+            if stack:
+                low, high = -1, len(stack)
+                while low + 1 < high:
+                    mid = (low + high) // 2
+                    if prefix[i] - prefix[stack[mid]] >= k:
+                        low = mid
+                    else:
+                        high = mid
+                if low != -1:
+                    result = min(result, i - stack[low])
+            stack.append(i)
+        if result == float("inf"):
             result = -1
         return result
