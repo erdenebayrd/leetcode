064045818091e6c6ae16ffa57ab837d15071dad9
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-07-20 15:43:53 +1000

    Time: 649 ms (5%), Space: 23.9 MB (37.78%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -1,38 +1,23 @@
-from collections import deque 
+from queue import Queue
 
 class Solution:
     def canReach(self, s: str, min_jump: int, max_jump: int) -> bool:
-        """
-        first observation is the last index is 0 or 1. if it's 1, no chance to reach there
-        second thing is
-            * we need to never visit each index more than once
-            * but also don't need to check every index is visited or not, it will take time
-            * to do that we can take care of latest index we reached, lets call it "right"
-            * for example we are at the index "i" we can jump into [i + min_jump -> i + max_jump]
-                if "right" index is greater than i + min_jump, we don't need to check [i + min_jump -> right] since we already know we reached nodes (indices) between them
-            * initially "right" is 0 since we are already at index 0
-        
-        so we can do in bfs so that time and space complexity would be O(N) N is length of string s
-        """
-        # time: O(N)
+        # time: O(N) N is length of string s
         # space: O(N)
-        # method: BFS + sliding window ( right most node )
-        if s[-1] == '1':
-            return False
-        
+        # method: bfs   
+        queue = Queue()
+        queue.put(0)
         n = len(s)
-        queue = deque()
-        queue.append(0)
-        right = 0
-        while queue:
-            node = queue.popleft()
-            if node == n - 1:
-                return True
-            start = max(right + 1, node + min_jump)
-            end = min(n - 1, node + max_jump)
-            for neighbor in range(start, end + 1):
-                if s[neighbor] == '0':
-                    queue.append(neighbor)
-            right = max(right, node + max_jump)
+        left = right = -1
 
+        while not queue.empty():
+            index = queue.get()
+            left = max(right + 1, index + min_jump)
+            right = max(right, index + max_jump)
+            for i in range(left, min(n, right + 1)):
+                if s[i] == '1':
+                    continue
+                if n - 1 == i:
+                    return True
+                queue.put(i)
         return False
