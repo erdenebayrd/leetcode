255a43d5610e7a5f4cf8c707dcc937e9c17454f8
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-07-20 14:44:51 +1000

    Time: 115 ms (20.47%), Space: 21.3 MB (75.63%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,31 +1,29 @@
-from functools import lru_cache
-
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        
-        # time: O(N * M)
-        # space: O(N * M)
-        # method: DP
-        
+        """
+        to find min number of deletions from both string to make them equal
+        means, to find longest common substring of them. Then add absolute diff between them.
+        formally
+        min_distance = len(word1) - lcs + len(word2) - lcs = len(word1) + len(word2) - 2 * lcs
+        to find lcs, we can use 2 dimension DP
+        """
+        # time: O(n * m)
+        # space: O(n * m)
+        # method: DP LCS
         n = len(word1)
         m = len(word2)
-        
         dp = [[0] * m for _ in range(n)]
-        
         lcs = 0
         for i in range(n):
             for j in range(m):
-                if j - 1 >= 0:
-                    dp[i][j] = max(dp[i][j], dp[i][j - 1])
-                if i - 1 >= 0:
+                if i > 0:
                     dp[i][j] = max(dp[i][j], dp[i - 1][j])
+                if j > 0:
+                    dp[i][j] = max(dp[i][j], dp[i][j - 1])
                 if word1[i] == word2[j]:
-                    if i - 1 >= 0 and j - 1 >= 0:
-                        dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
-                    else:
+                    if i == 0 or j == 0:
                         dp[i][j] = 1
-                lcs = max(lcs, dp[i][j])
-        # print(dp)
-        # print(lcs)
-        result = n + m - 2 * lcs
-        return result
+                    else:
+                        dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
+            lcs = max(lcs, dp[i][j])
+        return len(word1) + len(word2) - 2 * lcs
