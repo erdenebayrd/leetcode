8265d819ce9ece34ad4e8e0c66b1ff392399f151
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-07-21 17:27:13 +1000

    Time: 23 ms (77.44%), Space: 33.7 MB (30.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -2,13 +2,12 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # time: O(N)
         # space: O(1)
-        # method: Tortoise and Hare algorithm (Floyd cycle detection algorightm by slow and fast pointer)
+        # method: Tortoise and Hare algo
         slow = fast = 0
         while slow == 0 or slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
-        
-        slow = 0
+        fast = 0
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
