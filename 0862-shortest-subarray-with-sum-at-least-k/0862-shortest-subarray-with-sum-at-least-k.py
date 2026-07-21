3a4947dbb1c8b237813c421d5c2631ef74b3b7ce
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-07-21 20:07:34 +1000

    Time: 122 ms (89.05%), Space: 26.3 MB (87.29%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         """
@@ -26,32 +28,59 @@ class Solution:
         |
         -------------------------------------------->
         """
-        # time: O(N log N)
+        # # time: O(N log N)
+        # # space: O(N)
+        # # method: monotinic stack + binary search
+        # n = len(nums)
+        # prefix = [0] * n
+        # prefix[0] = nums[0]
+        # for i in range(1, n):
+        #     prefix[i] = nums[i] + prefix[i - 1]
+        # result = float('inf')
+        # stack = [] # indices of prefix array
+        # for i in range(n):
+        #     if prefix[i] >= k:
+        #         result = min(result, i + 1)
+        #     low, high = -1, len(stack)
+        #     while low + 1 < high:
+        #         mid = (low + high) // 2
+        #         if prefix[i] - prefix[stack[mid]] >= k:
+        #             low = mid
+        #         else:
+        #             high = mid
+        #     if low != -1:
+        #         result = min(result, i - stack[low])
+
+        #     while stack and prefix[stack[-1]] >= prefix[i]:
+        #         stack.pop()
+        #     stack.append(i)
+        # if result == float('inf'):
+        #     result = -1
+        # return result
+
+        """
+            improvement is if there is some j index found before i, that j index never will be used again after i since we are handling strictly increasing monotonic stack/queue
+        """
+        # time: O(N)
         # space: O(N)
-        # method: monotinic stack + binary search
+        # method: monotonic increasing queue
         n = len(nums)
         prefix = [0] * n
         prefix[0] = nums[0]
         for i in range(1, n):
             prefix[i] = nums[i] + prefix[i - 1]
+        queue = deque() # indices of prefix array
         result = float('inf')
-        stack = [] # indices of prefix array
         for i in range(n):
             if prefix[i] >= k:
                 result = min(result, i + 1)
-            low, high = -1, len(stack)
-            while low + 1 < high:
-                mid = (low + high) // 2
-                if prefix[i] - prefix[stack[mid]] >= k:
-                    low = mid
-                else:
-                    high = mid
-            if low != -1:
-                result = min(result, i - stack[low])
-
-            while stack and prefix[stack[-1]] >= prefix[i]:
-                stack.pop()
-            stack.append(i)
+            while queue and prefix[i] - prefix[queue[0]] >= k:
+                result = min(result, i - queue[0])
+                queue.popleft()
+            
+            while queue and prefix[queue[-1]] >= prefix[i]:
+                queue.pop()
+            queue.append(i)
         if result == float('inf'):
             result = -1
         return result
