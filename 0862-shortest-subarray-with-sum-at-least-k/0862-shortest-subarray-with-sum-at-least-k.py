5dff71925fac967a8bb69ee5b68e9e0fa860fd58
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-07-21 19:58:56 +1000

    Time: 590 ms (5.15%), Space: 26.8 MB (80.39%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,35 +1,57 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
+        """
+        since the array can contains negative numbers, we can't do sliding window
+
+        instead we can create a prefix sum array 
+        p0, p1, p2, .....pi.. , pn - 1    
+                          ^
+        pi - pj >= k      closest j before i
+        pj <= pi - k
+        so, we need to create a monotonic strictly increasing array by prefix array
+
+        ^
+        |
+        |                                            [*]
+        |                                            /
+        |           [*] ------       V              /
+        |          /         ------ [*] ----       /
+        |         /                         ---- [*] 
+        |        /                     
+        |      [*]                
+        |      /
+        |    [*]
+        |
+        |
+        |
+        -------------------------------------------->
+        """
         # time: O(N log N)
-        # space O(N)
-        # method: monotonic strictly increasing stack + binary search
+        # space: O(N)
+        # method: monotinic stack + binary search
         n = len(nums)
         prefix = [0] * n
         prefix[0] = nums[0]
-        if prefix[0] >= k:
-            return 1
-        
-        result = float('inf')
         for i in range(1, n):
-            prefix[i] += prefix[i - 1] + nums[i]
+            prefix[i] = nums[i] + prefix[i - 1]
+        result = float('inf')
+        stack = [] # indices of prefix array
+        for i in range(n):
             if prefix[i] >= k:
                 result = min(result, i + 1)
-        
-        stack = [] # contains index only
-        for i in range(n):
+            low, high = -1, len(stack)
+            while low + 1 < high:
+                mid = (low + high) // 2
+                if prefix[i] - prefix[stack[mid]] >= k:
+                    low = mid
+                else:
+                    high = mid
+            if low != -1:
+                result = min(result, i - stack[low])
+
             while stack and prefix[stack[-1]] >= prefix[i]:
                 stack.pop()
-            if stack:
-                low, high = -1, len(stack)
-                while low + 1 < high:
-                    mid = (low + high) // 2
-                    if prefix[i] - prefix[stack[mid]] >= k:
-                        low = mid
-                    else:
-                        high = mid
-                if low != -1:
-                    result = min(result, i - stack[low])
             stack.append(i)
-        if result == float("inf"):
+        if result == float('inf'):
             result = -1
         return result
