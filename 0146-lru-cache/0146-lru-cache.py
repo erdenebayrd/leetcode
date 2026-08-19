585f74afa3654a7324a38fdd9bf82148a03ff98d
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-08-19 22:40:21 +1000

    Time: 54 ms (96.98%), Space: 29.6 MB (97.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,71 +1,71 @@
 class Node:
-    def __init__(self, key=None, value=None, next=None, prev=None) -> None:
+    def __init__(self, key=None, value=None) -> None:
         self.key = key
         self.value = value
-        self.prev = prev
-        self.next = next
+        self.next = None
+        self.prev = None
+    
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.keys = {}
+    
+    def __delete(self, key: int) -> None:
+        if key not in self.keys:
+            return
+        node = self.keys[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        del self.keys[key]
+    
+    def __append(self, key: int, value: int) -> None:
+        node = Node(key=key, value=value)
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+        self.keys[key] = node
+
+    def get(self, key: int) -> int:
+        if key not in self.keys:
+            return -1
+        value = self.keys[key].value
+        self.__delete(key)
+        self.__append(key, value)
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.keys:
+            self.__delete(key)
+        self.__append(key, value)
+
+    def popleft(self) -> None:
+        if self.head.next == self.tail:
+            return
+        node = self.head.next
+        del self.keys[node.key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def __len__(self) -> int:
+        return len(self.keys)
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.tail = self.head = Node()
-    
-        self.size = 0
         self.capacity = capacity
-        self.hashmap = {}        
-
-    def append(self, key: int, value: int) -> None:
-        node = Node(key=key, value=value)
-        self.tail.next = node
-        node.prev = self.tail
-        self.tail = node
-        self.hashmap[key] = self.tail
-        self.size += 1
-
-    def moveToEnd(self, key: int) -> None:
-        currentNode = self.hashmap[key]
-        
-        prevNode = currentNode.prev
-        nextNode = currentNode.next
-        if nextNode is None: # already at end
-            return
-        
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-
-        self.tail.next = currentNode
-        currentNode.prev = self.tail
-        currentNode.next = None
-
-        self.tail = currentNode
-
-    def popleft(self) -> None:
-        node = self.head.next
-        nextNode = node.next
-        self.head.next = nextNode
-        if nextNode is not None:
-            nextNode.prev = self.head
-        
-        node.prev = None
-        node.next = None
-        del self.hashmap[node.key]
-        self.size -= 1
+        self.linked_list = LinkedList()
 
     def get(self, key: int) -> int:
-        if key not in self.hashmap:
-            return -1
-        self.moveToEnd(key)
-        return self.hashmap[key].value
+        return self.linked_list.get(key)
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.hashmap:
-            self.append(key=key, value=value)
-        else:
-            self.hashmap[key].value = value
-            self.moveToEnd(key)
-        
-        while self.size > self.capacity:
-            self.popleft()
+        self.linked_list.put(key, value)
+        if len(self.linked_list) > self.capacity:
+            self.linked_list.popleft()
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
