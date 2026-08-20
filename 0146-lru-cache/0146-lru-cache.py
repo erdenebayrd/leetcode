0e01d2fad6dc309805afb22c1ca3e9cc74ea6a6b
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-08-20 14:41:10 +1000

    Time: 16 ms (99.51%), Space: 29.2 MB (99.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,71 +1,22 @@
-class Node:
-    def __init__(self, key=None, value=None) -> None:
-        self.key = key
-        self.value = value
-        self.next = None
-        self.prev = None
-    
-class LinkedList:
-    def __init__(self) -> None:
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.keys = {}
-    
-    def __delete(self, key: int) -> None:
-        if key not in self.keys:
-            return
-        node = self.keys[key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        del self.keys[key]
-    
-    def __append(self, key: int, value: int) -> None:
-        node = Node(key=key, value=value)
-        self.tail.prev.next = node
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
-        self.keys[key] = node
-
-    def get(self, key: int) -> int:
-        if key not in self.keys:
-            return -1
-        value = self.keys[key].value
-        self.__delete(key)
-        self.__append(key, value)
-        return value
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.keys:
-            self.__delete(key)
-        self.__append(key, value)
-
-    def popleft(self) -> None:
-        if self.head.next == self.tail:
-            return
-        node = self.head.next
-        del self.keys[node.key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    def __len__(self) -> int:
-        return len(self.keys)
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.ordered_dict = OrderedDict()
         self.capacity = capacity
-        self.linked_list = LinkedList()
 
     def get(self, key: int) -> int:
-        return self.linked_list.get(key)
+        if key not in self.ordered_dict:
+            return -1
+        self.ordered_dict.move_to_end(key, last=True)
+        return self.ordered_dict.get(key)
 
     def put(self, key: int, value: int) -> None:
-        self.linked_list.put(key, value)
-        if len(self.linked_list) > self.capacity:
-            self.linked_list.popleft()
+        self.ordered_dict[key] = value
+        self.ordered_dict.move_to_end(key, last=True)
+        if len(self.ordered_dict) > self.capacity:
+            self.ordered_dict.popitem(last=False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
