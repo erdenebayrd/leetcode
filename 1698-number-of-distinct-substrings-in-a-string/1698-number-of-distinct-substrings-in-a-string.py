ee060952554fcd1e067a86627a228f8ca80990fb
erdenebayrd <31505361+erdenebayrd@users.noreply.github.com>
2026-08-26 15:09:30 +1000

    Time: 43 ms (92.31%), Space: 19.4 MB (97.44%) - LeetHub

--- 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
+++ 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
@@ -1,12 +1,67 @@
 class Solution:
     def countDistinct(self, s: str) -> int:
-        # time: O(N ^ 3)
+        # time: O(N log N)
         # space: O(N)
-        # method: brute force
+        # method: Suffix Array LCP
 
+        def build_sa(text: str) -> list:
+            n = len(text)
+            ranks = [(ord(text[i]), -1, i) for i in range(n)]
+            ranks.sort()
+            for bitmask in range(n.bit_length()):
+                updated_ranks = [0] * n
+                pos = [0] * n # ranks position by suffix index
+                for i in range(1, n):
+                    updated_ranks[i] = updated_ranks[i - 1]
+                    if ranks[i - 1][0] != ranks[i][0] or ranks[i - 1][1] != ranks[i][1]:
+                        updated_ranks[i] += 1
+                    pos[ranks[i][2]] = i
+                
+                for i in range(n):
+                    j = ranks[i][2] + (1 << bitmask)
+                    rank = -1
+                    if j < n:
+                        rank = updated_ranks[pos[j]]
+                    ranks[i] = (updated_ranks[i], rank, ranks[i][2])
+                
+                ranks = sorted(ranks) # O(N log N)
+                # ranks = radix_sort(ranks) # TODO: implement radix sort O(N)
+            return [i for _, _, i in ranks]
+
+        def build_lcp(sa: list, text: str) -> list:
+            n = len(sa)
+            lcp = [0] * n
+            pos = [0] * n
+            for i in range(n):
+                pos[sa[i]] = i
+            
+            prev_matched = 0
+            for i in range(n):
+                curr = pos[i]
+                prev = curr - 1
+                if prev == -1:
+                    prev_matched = 0
+                    continue
+
+                matched = 0
+                if prev_matched:
+                    matched = prev_matched - 1
+                
+                while sa[curr] + matched < n and sa[prev] + matched < n and text[sa[curr] + matched] == text[sa[prev] + matched]:
+                    matched += 1
+                lcp[curr] = matched
+                prev_matched = matched
+            
+            return lcp
+        
         n = len(s)
-        seen = set()
-        for left in range(n):
-            for right in range(left, n):
-                seen.add(s[left: right + 1])
-        return len(seen)
+        sa = build_sa(s)
+        lcp = build_lcp(sa, s)
+
+        # for i in range(n):
+        #     print(lcp[i], sa[i], s[sa[i]:])
+
+        result = 0
+        for i in range(n):
+            result += n - sa[i] - lcp[i]
+        return result
